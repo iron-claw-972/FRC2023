aca7d34b61e5b9526ae5bf5dc5dae81ded79a983
GLRoylance <GLRoylance@gmail.com>
2023-01-29 17:17:46 -0800

    add comments to trivial test

--- src/test/java/frc/robot/util/ArithTest.java
+++ src/test/java/frc/robot/util/ArithTest.java
@@ -4,8 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.*;
 
+/**
+ * Example of a JUnit test class.
+ * This test should run everytime someone builds the robot code.
+ */
 public class ArithTest {
 
+    /**
+     * Test if floating point addition works.
+     */
     @Test
     public void testSimpleArith() {
         assertEquals("addition does not work", 5.0, 2.0 + 3.0,  0.0001);
