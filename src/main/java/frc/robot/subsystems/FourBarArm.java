cb97d11bffc7c3f47b0ce519583fc0ba9ab318e3
MagicEmu <yichendai3@gmail.com>
2023-02-03 15:21:41 -0800

    added encoder object

--- src/main/java/frc/robot/subsystems/FourBarArm.java
+++ src/main/java/frc/robot/subsystems/FourBarArm.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
@@ -9,19 +10,19 @@ import frc.robot.constants.Constants;
 public class FourBarArm extends SubsystemBase {
   private final CANSparkMax m_motor;
   private final SparkMaxPIDController m_pid;
+  private final RelativeEncoder m_encoder;
   private double armSetpoint = Constants.arm.initialPosition;
   public FourBarArm() {
     m_motor = new CANSparkMax(Constants.arm.motorID, MotorType.kBrushless);
+    m_encoder = m_motor.getEncoder();
     m_pid = m_motor.getPIDController();
     m_pid.setP(Constants.arm.kP);
     m_pid.setI(Constants.arm.kI);
     m_pid.setD(Constants.arm.kD);
+    m_pid.setReference(armSetpoint, CANSparkMax.ControlType.kPosition);
   }
   public void setSetpoint(double target) {
     armSetpoint = target;
-  }
-  @Override
-  public void periodic() {
     m_pid.setReference(armSetpoint, CANSparkMax.ControlType.kPosition);
   }
 }
