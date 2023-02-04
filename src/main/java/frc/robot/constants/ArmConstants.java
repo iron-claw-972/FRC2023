87569accf6fcdd30ea442d051a629471a9bb8bd9
MagicEmu <yichendai3@gmail.com>
2023-02-04 13:26:48 -0800

    started arm sim

--- src/main/java/frc/robot/constants/ArmConstants.java
+++ src/main/java/frc/robot/constants/ArmConstants.java
@@ -1,15 +1,28 @@
 package frc.robot.constants;
 
+import edu.wpi.first.math.system.plant.DCMotor;
 import frc.robot.Robot;
 
 public class ArmConstants {
+  // arm ids
   public final int motorID = -1;
+
+  // pid values
   public final double kP = -1;
   public final double kI = -1;
   public final double kD = -1;
+
+  // distance values
   public final double initialPosition = 0;
   public final double shelfPosition = -1;
   public final double intakePosition = -1;
   public final double middlePosiiton = -1;
   public final double topPosition = -1;
+
+  // simulation constants
+  public final DCMotor armSimMotor = DCMotor.getNEO(1);
+  public final double armReduction = 45;
+  public final double armMass = 2.05;
+  public final double armLength = .24;
+  public final double armMOI = .1194;
 }
--- src/main/java/frc/robot/subsystems/FourBarArm.java
+++ src/main/java/frc/robot/subsystems/FourBarArm.java
@@ -1,9 +1,13 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.util.Units;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxAlternateEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 
@@ -12,9 +16,24 @@ public class FourBarArm extends SubsystemBase {
   private final SparkMaxPIDController m_pid;
   private final RelativeEncoder m_encoder;
   private double armSetpoint = Constants.arm.initialPosition;
+
+  private final SingleJointedArmSim m_armSim = 
+    new SingleJointedArmSim(
+      Constants.arm.armSimMotor, 
+      Constants.arm.armReduction, 
+      Constants.arm.armMOI, 
+      Constants.arm.armLength, 
+      Units.degreesToRadians(0), 
+      Units.degreesToRadians(180), 
+      true
+      );
+  private final EncoderSim m_encoderSim = new EncoderSim(m_encoder);
+  private double armPositionDeg = 0;
+  private double kArmEncoderDistPerPulse = 2.0*Math.PI/8192;
+
   public FourBarArm() {
     m_motor = new CANSparkMax(Constants.arm.motorID, MotorType.kBrushless);
-    m_encoder = m_motor.getEncoder();
+    m_encoder = m_motor.getAlternateEncoder(SparkMaxAlternateEncoder.Type.kQuadrature, 8192);
     m_pid = m_motor.getPIDController();
     m_pid.setP(Constants.arm.kP);
     m_pid.setI(Constants.arm.kI);
