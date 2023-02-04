cd7332fc22481efdca7148f32f9fffb542356500
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2023-02-03 16:57:04 -0800

    Individual characterization - by Michael C

--- src/main/java/frc/robot/commands/SteerFeedForwardCharacterzation.java
+++ src/main/java/frc/robot/commands/SteerFeedForwardCharacterzation.java
@@ -9,23 +9,25 @@ package frc.robot.commands;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Module;
 import frc.robot.util.FeedForwardCharacterizationData;
+import frc.robot.util.ShuffleboardManager;
 
 
 /** Add your docs here. */
 public class SteerFeedForwardCharacterzation extends CommandBase {
   double value = 0;
-  FeedForwardCharacterizationData[] m_feedForwardCharacterizationData;
-  
+  FeedForwardCharacterizationData m_feedForwardCharacterizationData;
+  Module m_module;
 
   Timer m_timer = new Timer();
   Drivetrain m_drive;
 
   public SteerFeedForwardCharacterzation(Drivetrain drive) {
-    
     this.m_drive = drive;
     addRequirements(drive);
   }
@@ -33,65 +35,66 @@ public class SteerFeedForwardCharacterzation extends CommandBase {
   public void initialize() {
     m_timer.start();
     m_drive.setAllOptimize(false);
-    m_feedForwardCharacterizationData = new FeedForwardCharacterizationData[] {
-      new FeedForwardCharacterizationData(),
-      new FeedForwardCharacterizationData(),
-      new FeedForwardCharacterizationData(),
-      new FeedForwardCharacterizationData()
-    };
+    m_feedForwardCharacterizationData = new FeedForwardCharacterizationData();
+    this.m_module= Robot.shuffleboard.getModule();
   }
 
   public void execute() {
-    runcharacterazationVolts();
-    if (m_timer.get() > 0.25) {
-      for (int i=0; i<4; i++){
-        m_feedForwardCharacterizationData[i].add(Robot.drive.m_modules[i].getSteerVelocity(), value); 
-      }
+    runCharacterizationVolts();
+    if (m_timer.get() > 0.5) {
+        
+          m_feedForwardCharacterizationData.add(m_module.getSteerVelocity(), value);
+
+        
       
     }
-    if (m_timer.get() >= 1.0) {
+    if (m_timer.get() > 1.5) {
       value += 0.1;
       m_timer.reset();
       m_timer.start();
       System.out.println(value);
     }
 
+    
+
   }
 
-  private void runcharacterazationVolts() {
-    for (int i = 0; i < 4; i++) {
-      Robot.drive.m_modules[i].setDriveVoltage(0);
-      Robot.drive.m_modules[i].setSteerVoltage(value);
-    }
+  private void runCharacterizationVolts() {
+
+    m_module.setDriveVoltage(0);
+    
+    m_module.setSteerVoltage(value);
+      
+      
+    
   }
 
   public void end(boolean interrupted) {
     System.out.println("FINISHED");
-    for (int i=0; i<4; i++){
-      m_feedForwardCharacterizationData[i].print();
+ 
+      m_feedForwardCharacterizationData.print();
     
-    }
     
-    for (int i=0; i<4;i++){
-      Robot.shuffleboard.m_steerStaticFeedForwardSaver.replace(Robot.drive.m_modules[i], m_feedForwardCharacterizationData[i].getSatic());
-      Robot.shuffleboard.m_steerVelFeedForwardSaver.replace(Robot.drive.m_modules[i], m_feedForwardCharacterizationData[i].getVelocity());
-      System.out.println("Static " + i + ": " + m_feedForwardCharacterizationData[i].getSatic());
-      System.out.println("Velocity " + i + ": " + m_feedForwardCharacterizationData[i].getVelocity());
+    
+      Robot.shuffleboard.m_steerStaticFeedForwardSaver.replace(m_module, m_feedForwardCharacterizationData.getSatic());
+      Robot.shuffleboard.m_steerVelFeedForwardSaver.replace(m_module, m_feedForwardCharacterizationData.getVelocity());
+      System.out.println("Static " + ": " + m_feedForwardCharacterizationData.getSatic());
+      System.out.println("Velocity " + ": " + m_feedForwardCharacterizationData.getVelocity());
       
 
 
-    }
     
-    for (int i = 0; i < 4; i++) {
-      Robot.drive.m_modules[i].setDriveVoltage(0);
-      Robot.drive.m_modules[0].setSteerAngle(new Rotation2d(Units.degreesToRadians(135)));
-    }
+    
+    
+      m_module.setDriveVoltage(0);
+      m_module.setSteerVoltage(0);
+    
     
   }
 
   public boolean isFinished() {
     //System.out.println(value > 11);
-    return value > 6;
+    return value>6;
   }
 
   
