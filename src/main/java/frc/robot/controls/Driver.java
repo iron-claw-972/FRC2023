7dde1d0e5bd70dcc30a41cc41f2987cc15103ca8
zhoutony05 <63679813+zhoutony05@users.noreply.github.com>
2023-02-11 11:52:30 -0800

    Update Driver.java

--- src/main/java/frc/robot/controls/Driver.java
+++ src/main/java/frc/robot/controls/Driver.java
@@ -1,5 +1,4 @@
 package frc.robot.controls;
-
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
@@ -13,19 +12,5 @@ public class Driver {
   private static GameController driver = new GameController(Constants.oi.kDriverJoy);
 
   public static void configureControls() {
-    
-    // example button binding implementation
-    driver.get(Button.A).onTrue(new DoNothing());
-
-    // example test type implementation
-    // tests drivetrain, when in TEST_DRIVE test mode and 
-    driver.get(Button.A).and(Robot.shuffleboard.isTestTypeTrigger(TestType.TEST_DRIVE)).onTrue(
-      new SequentialCommandGroup(
-        new RunCommand(() -> Robot.drive.tankDrive(0.5, 0.5), Robot.drive).withTimeout(1),
-        new RunCommand(() -> Robot.drive.tankDrive(-0.5, -0.5), Robot.drive).withTimeout(1),
-        new RunCommand(() -> Robot.drive.tankDrive(0.5, -0.5), Robot.drive).withTimeout(1),
-        new RunCommand(() -> Robot.drive.tankDrive(-0.5, 0.5), Robot.drive).withTimeout(1)
-      )
-    );
   }
 }
