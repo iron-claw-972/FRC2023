2399ae9c0485aa9eb1fe1a8a0cb4a409ac512660
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2023-02-11 19:57:04 -0800

    accidently removed this

--- src/main/java/frc/robot/constants/AutoConstants.java
+++ src/main/java/frc/robot/constants/AutoConstants.java
@@ -2,6 +2,9 @@ package frc.robot.constants;
 
 public class AutoConstants {
 
+  // Pathplanner output folder should be src/main/deploy/pathplanner
+  public static final String kTrajectoryDirectory = "pathplanner/";
+
   public static final double kMaxAutoSpeed = 1.0; // m/s
   public static final double kMaxAutoAccel = 1.0; // m/s^2
 }
