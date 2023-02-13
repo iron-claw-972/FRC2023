8a3186818f3e2b3c0887403b95048f1573ca0e75
MagicEmu <yichendai3@gmail.com>
2023-02-13 15:46:32 -0800

    removed redundant command schedules

--- src/main/java/frc/robot/controls/Operator.java
+++ src/main/java/frc/robot/controls/Operator.java
@@ -1,6 +1,5 @@
 package frc.robot.controls;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.arm.ExtendToPosition;
 import frc.robot.constants.ArmConstants;
 import frc.robot.constants.OIConstants;
@@ -15,9 +14,9 @@ public class Operator {
    * Configures all of the operator controls.
    */
   public static void configureControls(FourBarArm arm) {
-    operator.get(Button.Y).onTrue(new SequentialCommandGroup(new ExtendToPosition(arm, ArmConstants.topPosition), new ExtendToPosition(arm, ArmConstants.initialPosition)));
-    operator.get(Button.X).onTrue(new SequentialCommandGroup(new ExtendToPosition(arm, ArmConstants.middlePosiiton), new ExtendToPosition(arm, ArmConstants.initialPosition)));
-    operator.get(Button.A).onTrue(new SequentialCommandGroup(new ExtendToPosition(arm, ArmConstants.intakePosition), new ExtendToPosition(arm, ArmConstants.initialPosition)));
-    operator.get(Button.Y).onTrue(new SequentialCommandGroup(new ExtendToPosition(arm, ArmConstants.shelfPosition), new ExtendToPosition(arm, ArmConstants.initialPosition)));
+    operator.get(Button.Y).onTrue(new ExtendToPosition(arm, ArmConstants.topPosition));
+    operator.get(Button.X).onTrue(new ExtendToPosition(arm, ArmConstants.middlePosiiton));
+    operator.get(Button.A).onTrue(new ExtendToPosition(arm, ArmConstants.intakePosition));
+    operator.get(Button.Y).onTrue(new ExtendToPosition(arm, ArmConstants.shelfPosition));
   }
 }
