b86c15ccf55503219faea8f26237d238462cd6e5
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2023-02-12 20:19:42 -0800

    camel case constants

--- src/main/java/frc/robot/constants/DeployingBarConstants.java
+++ src/main/java/frc/robot/constants/DeployingBarConstants.java
@@ -3,7 +3,7 @@ package frc.robot.constants;
 public class DeployingBarConstants {
   public static final int kLeftMotor = 0;
   public static final int kRightMotor = 0;
-  public static final int kmaxExtension = 0, kminExtension = 0;
+  public static final int kMaxExtension = 0, kMinExtension = 0;
   public static final int kP = 0, kI = 0, kD = 0;
 
 }
