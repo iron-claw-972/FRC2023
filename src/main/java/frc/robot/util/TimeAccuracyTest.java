1621ac28d455bd05f60bfb8c7b83d9577f1ffef9
omega <62168528+omegaladon@users.noreply.github.com>
2023-02-15 14:54:14 -0800

    Removed DoubleSupplier instance variables

--- src/main/java/frc/robot/util/TimeAccuracyTest.java
+++ src/main/java/frc/robot/util/TimeAccuracyTest.java
@@ -9,24 +9,11 @@ import edu.wpi.first.util.WPIUtilJNI;
  */
 public class TimeAccuracyTest {
   
-  private DoubleSupplier m_actual, m_setpoint;
   private BooleanSupplier m_accuracyTest;
   private double m_setpointUpdateTime;
   private double m_errorMargin;
   private double m_timeMargin;
   private boolean m_lastUseableResult = false;
-
-  /**
-   * @param accuracyTest BooleanSupplier that returns true if the test is accurate
-   * @param setpoint DoubleSupplier that returns the setpoint
-   * @param timeMargin time in seconds that the setpoint must be held for the test to be accurate
-   */
-  public TimeAccuracyTest(BooleanSupplier accuracyTest, DoubleSupplier setpoint, double timeMargin){
-    m_setpoint = setpoint;
-    m_timeMargin = timeMargin;
-    m_setpointUpdateTime = WPIUtilJNI.now() * 1e-6;
-    m_accuracyTest =  accuracyTest;
-  }
 
   /**
    * @param actual DoubleSupplier that returns the actual value
@@ -35,12 +22,15 @@ public class TimeAccuracyTest {
    * @param timeMargin time in seconds that the setpoint must be held for the test to be accurate
    */
   public TimeAccuracyTest(DoubleSupplier actual, DoubleSupplier setpoint, double errorMargin, double timeMargin){
-    m_actual = actual;
-    m_setpoint = setpoint;
     m_errorMargin = errorMargin;
     m_timeMargin = timeMargin;
     m_setpointUpdateTime = WPIUtilJNI.now() * 1e-6;
-    m_accuracyTest =  this::getDoubleAccuracyTest;
+    m_accuracyTest =  new BooleanSupplier(){
+      @Override
+      public boolean getAsBoolean() {
+        return getDoubleAccuracyTest(actual, setpoint);
+      }
+    };
   }
 
   /**
@@ -53,10 +43,10 @@ public class TimeAccuracyTest {
   }
 
   /**
-   * Converts the double suppliers to a boolean.
+   * Determines if the actual value is within the error margin of the setpoint.
    * @return true if the actual value is within the error margin of the setpoint, false if not
    */
-  private boolean getDoubleAccuracyTest(){
-    return Math.abs(m_actual.getAsDouble() - m_setpoint.getAsDouble()) <= m_errorMargin;
+  private boolean getDoubleAccuracyTest(DoubleSupplier actual, DoubleSupplier setpoint){
+    return Math.abs(actual.getAsDouble() - setpoint.getAsDouble()) <= m_errorMargin;
   }
 }
