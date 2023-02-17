4cfbed2f0d1e2985cb8d81c543905e9ffb62ce2a
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2023-02-17 13:40:53 -0800

    change constant IDs

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -4,6 +4,6 @@ public class DriveConstants {
 
   public static final int kLeftMotor1 = 1;
   public static final int kLeftMotor2 = 2;
-  public static final int kRightMotor1 = 35;
+  public static final int kRightMotor1 = 3;
   public static final int kRightMotor2 = 4;
 }
--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -2,9 +2,9 @@ package frc.robot.constants;
 
 public final class IntakeConstants {
   
-  public static final int kLeftMotorPort = 1;
+  public static final int kLeftMotorPort = 6;
 
-  public static final int kRightMotorPort = 19;
+  public static final int kRightMotorPort = 7;
 
   public static final double kIntakeSpeed = 0.3;
 
