0f5f37be74546f9b340e2e66c60b4da4336ee98f
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2023-02-17 13:31:47 -0800

    remove rotate() and unused imports

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,18 +7,11 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.Constants;
 import frc.robot.constants.IntakeConstants;
-import frc.robot.controls.Driver;
-import frc.robot.util.MotorFactory;
-import lib.controllers.GameController.Axis;
 
 public class Intake extends SubsystemBase {
 
@@ -26,26 +19,19 @@ public class Intake extends SubsystemBase {
   private final CANSparkMax rightMotor;
 
   public Intake() {
-
     leftMotor = new CANSparkMax(IntakeConstants.kLeftMotorPort, MotorType.kBrushless);
     rightMotor = new CANSparkMax(IntakeConstants.kRightMotorPort, MotorType.kBrushless);
     leftMotor.restoreFactoryDefaults();
     rightMotor.restoreFactoryDefaults();
-
   }
 
   public void intake(double speed) {
-    leftMotor.set(-speed);
+    leftMotor.set(speed);
     rightMotor.set(speed);
   }
 
   public void stop() {
     leftMotor.set(0);
     rightMotor.set(0);
-  }
-
-  public void rotate(double speed) {
-    leftMotor.set(speed);
-    rightMotor.set(speed);
   }
 } 
