77c9a2d5baf845a819a75cdb78977f0414cde5f0
lakshag32 <lakshag32@gmail.com>
2023-02-17 17:09:24 -0800

    add functionality to prevent motors from spinning after the top or bottom limit switch has been tripped

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -29,6 +29,8 @@ public class Elevator extends SubsystemBase {
   private final PIDController m_elevatorPID;
   private final DutyCycleEncoder m_absoluteSpoolEncoder;     
   private final TalonEncoder m_elevatorMotorEncoder; 
+  private final DigitalInput m_topLimitSwitch; 
+  private final DigitalInput m_bottomLimitSwitch; 
   private double m_absEncoderZeroPositionTicks;
   private double clampLow = -ElevatorConstants.kMotorLimit; 
   private double clampHigh = ElevatorConstants.kMotorLimit;
@@ -36,13 +38,14 @@ public class Elevator extends SubsystemBase {
 
   public Elevator() {
     m_motor = MotorFactory.createTalonFX(ElevatorConstants.kMotorPort, Constants.kRioCAN);
-    m_motor.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0);
-    m_motor.configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0);
+    //m_motor.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0);
+    //m_motor.configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0);
     m_absoluteSpoolEncoder = new DutyCycleEncoder(ElevatorConstants.kAbsEncoderPort); 
     m_elevatorMotorEncoder = new TalonEncoder(m_motor); 
     m_elevatorMotorEncoder.setDistancePerPulse(ElevatorConstants.kDistPerMotorEncoderTick);
     m_elevatorPID = new PIDController(ElevatorConstants.kP, ElevatorConstants.kI, ElevatorConstants.kD);  
-  
+    m_topLimitSwitch = new DigitalInput(ElevatorConstants.kTopLimitSwitchPort); 
+    m_bottomLimitSwitch = new DigitalInput(ElevatorConstants.kBottomLimitSwitchPort); 
     //TODO: log, addDouble doesn't work. 
     //LogManager.addDouble("Elevator/error", () -> {return m_pid.getSetpoint() - getElevatorHeight();});
   }
@@ -78,8 +81,19 @@ public class Elevator extends SubsystemBase {
   }
 
   public void set(double power){
-    m_motor.set(power); 
+    if(m_motor.get() > 0 && getBottomLimitSwitch() == true){
+      m_motor.set(power); 
+    }
+  
+    if(m_motor.get() <0 && getTopLimitSwitch() == true){
+      m_motor.set(power); 
+    }
+
+    else{
+      m_motor.set(0); 
+    }
   }
+  
   public void enableDisablePID(boolean condition){
     if(condition){
       m_enabled = true; 
@@ -96,18 +110,26 @@ public class Elevator extends SubsystemBase {
     m_elevatorPID.setSetpoint(setPointMeters);
   }
 
+  // public boolean getBottomLimitSwitch(){
+  //  if(m_motor.getSensorCollection().isRevLimitSwitchClosed() == 0){
+  //   return true; 
+  //  }
+  //   return false; 
+  // }
+
+  // public boolean getTopLimitSwitch(){
+  //   if(m_motor.getSensorCollection().isFwdLimitSwitchClosed() == 0){
+  //    return true; 
+  //   }
+  //    return false; 
+  // }
+
   public boolean getBottomLimitSwitch(){
-   if(m_motor.getSensorCollection().isRevLimitSwitchClosed() == 0){
-    return true; 
-   }
-    return false; 
+    return m_bottomLimitSwitch.get(); 
   }
 
   public boolean getTopLimitSwitch(){
-    if(m_motor.getSensorCollection().isFwdLimitSwitchClosed() == 0){
-     return true; 
-    }
-     return false; 
+    return m_topLimitSwitch.get(); 
   }
 
   public void resetMotorEncoder(){
