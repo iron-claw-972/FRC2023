86158590cec9e0b2101b8e57db141821fc95c657
MagicEmu <yichendai3@gmail.com>
2023-02-18 11:38:51 -0800

    removed feedforward from shuffleboard due to incompatibility

--- src/main/java/frc/robot/subsystems/FourBarArm.java
+++ src/main/java/frc/robot/subsystems/FourBarArm.java
@@ -85,7 +85,6 @@ public class FourBarArm extends SubsystemBase {
   public void setUpArmShuffleboard() {
     m_armTab.addNumber("Arm angle", () -> m_encoder.getPosition());
     m_armTab.addNumber("Motor output", () -> m_motor.get());
-    m_armTab.add("Feedforward", m_feedforward);
     m_armTab.add("PID", m_pid);
     m_armTab.add(m_pid);
   }
