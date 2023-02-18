e6893a99b1db888f4c7008c8b5fc28aefc845cbb
omega <62168528+omegaladon@users.noreply.github.com>
2023-02-18 14:22:57 -0800

    Update PolynomialRegression.java

--- src/main/java/lib/PolynomialRegression.java
+++ src/main/java/lib/PolynomialRegression.java
@@ -190,17 +190,4 @@ public class PolynomialRegression implements Comparable<PolynomialRegression> {
     }
     return 0;
   }
-
-  /**
-   * Unit tests the {@code PolynomialRegression} data type.
-   *
-   * @param args the command-line arguments
-   */
-  public static void main(String[] args) {
-    double[] x = {10, 20, 40, 80, 160, 200};
-    double[] y = {100, 350, 1500, 6700, 20160, 40000};
-    PolynomialRegression regression = new PolynomialRegression(x, y, 3);
-
-    System.out.println(regression);
-  }
 }
