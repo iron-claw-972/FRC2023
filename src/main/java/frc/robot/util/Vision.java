5e723ca820c968a6b691b62c20f9a5666c998131
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2023-02-19 13:27:35 -0800

    Move Vision.java from other branch

--- src/main/java/frc/robot/util/Vision.java
+++ src/main/java/frc/robot/util/Vision.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.util;
 
 import java.io.IOException;
@@ -12,129 +5,91 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
-import org.photonvision.RobotPoseEstimator;
-import org.photonvision.RobotPoseEstimator.PoseStrategy;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFieldLayout.OriginPosition;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import frc.robot.constants.FieldConstants;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.constants.VisionConstants;
-import frc.robot.subsystems.Drivetrain;
-
-
 
 public class Vision {
-  private static RobotPoseEstimator robotPoseEstimator;
-  private static AprilTagFieldLayout aprilTagFieldLayout;
-  private static Drivetrain drive;
- 
+  private AprilTagFieldLayout m_aprilTagFieldLayout;
+  private ArrayList<VisionCamera> m_cameras = new ArrayList<>();
+  private ShuffleboardTab m_shuffleboardTab;
 
   /**
    * Sets up the cameras and pose estimator
    * @param drive The drivetrain
+   * @param m_cameras The list of camera names and their translation from the center of robot
    */
-  public static void setup(Drivetrain drive) {
-    setup(drive, new PhotonCamera(VisionConstants.kCameraName1), new PhotonCamera(VisionConstants.kCameraName2));
-  }
+  public Vision(ShuffleboardTab shuffleboardTab, List<Pair<String, Transform3d>> camList) {
+    m_shuffleboardTab = shuffleboardTab;
 
-  /**
-   * Sets up the cameras and pose estimator
-   * @param drive The drivetrain
-   * @param camera1 The first camera
-   * @param camera2 The second camera
-   */
-  public static void setup(Drivetrain drive, PhotonCamera camera1, PhotonCamera camera2) {
-    Vision.drive=drive;
-    ArrayList<Pair<PhotonCamera, Transform3d>> camList;
-    if(VisionConstants.k2Cameras){
-      camList = new ArrayList<Pair<PhotonCamera, Transform3d>>(List.of(
-        new Pair<PhotonCamera, Transform3d>(camera1, VisionConstants.kRobotToCamera1),
-        new Pair<PhotonCamera, Transform3d>(camera2, VisionConstants.kRobotToCamera2)
-      ));
-    }else{
-      camList = new ArrayList<Pair<PhotonCamera, Transform3d>>(List.of(
-        new Pair<PhotonCamera, Transform3d>(camera1, VisionConstants.kRobotToCamera1)
-      ));
-    }
-    getTagFieldLayout();
-    aprilTagFieldLayout.setOrigin(AprilTagFieldLayout.OriginPosition.kBlueAllianceWallRightSide);
-    
-    robotPoseEstimator = new RobotPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camList);
-  }
-
-  /**
-   * Finds and returns the april tag field layout
-   * If it can't find it, it uses the april tags in the constants
-   * @return The field layout
-  */
-  public static AprilTagFieldLayout getTagFieldLayout() {
-    AprilTagFieldLayout aprilTagFieldLayout;
     try {
-      aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
-    } catch (IOException ex) {
-      aprilTagFieldLayout = new AprilTagFieldLayout(VisionConstants.kTagPoses, FieldConstants.kFieldLength, FieldConstants.kFieldWidth);
-      System.out.println("Vision setup IOException: "+ex.getMessage());
+      m_aprilTagFieldLayout = AprilTagFields.k2023ChargedUp.loadAprilTagLayoutField();
+    } catch (IOException e) {
+      m_aprilTagFieldLayout = new AprilTagFieldLayout(VisionConstants.kAprilTags, VisionConstants.kFieldLength, VisionConstants.kFieldWidth);
+      DriverStation.reportWarning("Could not find k2023ChargedUp.m_resourceFile, check that GradleRIO is updated to at least 2023.2.1 in build.gradle",  e.getStackTrace());
     }
-    aprilTagFieldLayout.setOrigin(AprilTagFieldLayout.OriginPosition.kBlueAllianceWallRightSide);
+    m_aprilTagFieldLayout.setOrigin(OriginPosition.kBlueAllianceWallRightSide);
 
-    return aprilTagFieldLayout;
-  }
-
-  /**
-   * Prints the estimated pose from the vision
-   */
-  public static void printEstimate(){
-    robotPoseEstimator.setReferencePose(new Pose3d(0, 0, 0, new Rotation3d(0, 0, 0)));
-    Optional<Pair<Pose3d,Double>> r = robotPoseEstimator.update();
-    if(r.isPresent()){
-      System.out.printf("Present: %b\nPose: (%.2f, %.2f, %.2f)\nRotation: %.2f degrees\nTime: %.2f\n",
-        r.isPresent(), 
-        r.get().getFirst().getX(), 
-        r.get().getFirst().getY(), 
-        r.get().getFirst().getZ(), 
-        r.get().getFirst().getRotation().toRotation2d().getDegrees(), 
-        r.get().getSecond()
-      );
-    }else{
-      System.out.println("Result is not present");
+    for (int i = 0; i < camList.size(); i++) {
+      m_cameras.add(this.new VisionCamera(camList.get(i).getFirst(), camList.get(i).getSecond()));
     }
   }
 
-  public static Optional<Pair<Pose3d,Double>> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
-    robotPoseEstimator.setReferencePose(prevEstimatedRobotPose);
-    Optional<Pair<Pose3d,Double>> r = robotPoseEstimator.update();
-    return r;
-}
-
-/**
- * Gets the pose as a Pose2d
- * @param referencePose The reference pose
- * @return The pose from vision as a Pose2d, null if it doesn't exist
- */
-public static Pose2d getPose2d(Pose2d referencePose){
-  Optional<Pair<Pose3d, Double>> p = getEstimatedGlobalPose(referencePose==null?drive.getPose():referencePose);
-  if(p.isPresent() && p.get().getFirst() != null && p.get().getSecond() != null && p.get().getFirst().getX() > -10000 && p.get().getSecond() >= 0){
-    return p.get().getFirst().toPose2d();
+  public ArrayList<EstimatedRobotPose> getEstimatedPoses(Pose2d referencePose) {
+    ArrayList<EstimatedRobotPose> estimatedPoses = new ArrayList<>();
+    for (int i = 0; i < m_cameras.size(); i++) {
+      Optional<EstimatedRobotPose> estimatedPose = m_cameras.get(i).getEstimatedPose(referencePose);
+      if (estimatedPose.isPresent()) {
+        estimatedPoses.add(estimatedPose.get());
+      }
+    }
+    return estimatedPoses;
   }
-  return null;
-}
 
-  public static AprilTagFieldLayout getAprilTagFieldLayout(){
-    return aprilTagFieldLayout;
+  public AprilTagFieldLayout getAprilTagFieldLayout(){
+    return m_aprilTagFieldLayout;
   }
 
   /**
-   * Returns the pose of an april tag
-   * @param id The id of the april tag
-   * @return The april tag pose as a pose3d
+   * @param id AprilTag id (1-8)
+   * @return Pose3d of the AprilTag
    */
-  public static Pose3d getTagPose(int id){
+  public Pose3d getTagPose(int id){
     return getAprilTagFieldLayout().getTagPose(id).get();
+  }
+
+  public void setupVisionShuffleboard() {
+  }
+
+  class VisionCamera {
+    PhotonCamera camera;
+    PhotonPoseEstimator photonPoseEstimator;
+  
+    public VisionCamera(String cameraName, Transform3d robotToCam) {
+      camera = new PhotonCamera(cameraName);
+      photonPoseEstimator = new PhotonPoseEstimator(m_aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP, camera, robotToCam);
+      photonPoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+    }
+  
+    /**
+     * @param referencePose previous estimated robot pose
+     * @return estimated robot pose
+     */
+    public Optional<EstimatedRobotPose> getEstimatedPose(Pose2d referencePose) {
+      photonPoseEstimator.setReferencePose(referencePose);
+      return photonPoseEstimator.update();
+    }
   }
 }
