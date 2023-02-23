92d4fe81400a518962c3614f9804a0f13bab1b37
Saara21 <pips7836@lgsstudent.org>
2023-02-23 15:59:09 -0800

    updated

--- src/main/java/frc/robot/commands/auto/IntakeBottomPieceCommand.java
+++ src/main/java/frc/robot/commands/auto/IntakeBottomPieceCommand.java
@@ -2,27 +2,28 @@ package frc.robot.commands.auto;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.FourBarArm;
+import frc.robot.util.PathGroupLoader;
 
 
 public class IntakeBottomPieceCommand extends SequentialCommandGroup{
     private Drivetrain m_drive;
     private FourBarArm m_Arm;
     
-    public IntakeBottomPieceCommand(){
-    
+    public IntakeBottomPieceCommand(Drivetrain m_drive, FourBarArm m_Arm){
         addCommands(
-        // deposit preloaded cone into high node of bottom grid
+        // ** deposit already loaded cone in the high node of the bottom grid.  
 
-        // **path not created yet, but have it drive to center (there is a structure of a path, but accuracy needs to be checked)
+       // first part of command makes it drive to center 
+        new PathPlannerCommand(PathGroupLoader.getPathGroup("To Center And Back"), 0, m_drive, true), 
+       
+        // ** intake a cone 
 
-        // intake cone game piece
+        // second part of command makes it drive back
+        new PathPlannerCommand(PathGroupLoader.getPathGroup("To Center And Back"), 1, m_drive, true)
 
-        // ** path not created yet, but have it drive back
+        // ** Deposit cone high nodes in the same grid and stop in original position
 
-        // deposit cone high nodes in same grid
         );
-            
-        
     }
 };
   
