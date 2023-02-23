ae541451ef3b333557f181662d5e3e65785a4a9f
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2023-02-23 15:02:05 -0800

    Update robot to camera

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -34,8 +34,8 @@ public class VisionConstants {
         new Pair<String, Transform3d>(
           "Camera_2",
           new Transform3d(
-            new Translation3d(-Units.inchesToMeters(4.75), Units.inchesToMeters(10.375), Units.inchesToMeters(10)),
-            new Rotation3d(0, 0, 0)
+            new Translation3d(Units.inchesToMeters(15), Units.inchesToMeters(-3.3125), Units.inchesToMeters(10)),
+            new Rotation3d(0, 0, Math.PI)
           )
         )
       ));
