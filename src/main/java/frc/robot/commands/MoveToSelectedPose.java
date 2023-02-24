ce7c4e062e8edbaf8c289b26f29eaa4e8cbc086c
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2023-02-23 16:59:19 -0800

    Use stop instead of drive

--- src/main/java/frc/robot/commands/MoveToSelectedPose.java
+++ src/main/java/frc/robot/commands/MoveToSelectedPose.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.controls.Operator;
 import frc.robot.subsystems.Drivetrain;
@@ -30,6 +29,6 @@ public class MoveToSelectedPose extends CommandBase {
   
   @Override
   public void end(boolean interrupted) {
-    m_drive.drive(0.0, 0.0, 0.0, false);
+    m_drive.stop();
   }
 }
