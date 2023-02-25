a4a76c2e8f39f8ae2a631daeb4813b4f96ca738f
Pennedrose <tamg6397@lgsstudent.org>
2023-02-25 11:35:42 -0800

    testing printcommands

--- src/main/java/frc/robot/commands/BalanceCommand.java
+++ src/main/java/frc/robot/commands/BalanceCommand.java
@@ -44,8 +44,9 @@ public class BalanceCommand extends CommandBase {
 
     @Override
     public void execute() {
-        System.out.println("Banana test");
         m_output = MathUtil.clamp(m_pid.calculate(m_currentAngle), -DriveConstants.kBalanceMaxOutput, DriveConstants.kBalanceMaxOutput);
+        
+        System.out.println(m_output);
         if(m_usePitch) {
             m_drive.driveHeading(m_output, 0, m_inverted*Math.PI/2, true);
         }
