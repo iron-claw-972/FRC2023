b30fc8ca955a31c0770c8c89470f41bcbdade546
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2023-02-24 22:59:04 -0800

    Robot.java cleaning

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.controls.Operator;
 import frc.robot.constants.Constants;
 import frc.robot.util.LogManager;
 
@@ -22,8 +21,6 @@ import frc.robot.util.LogManager;
 public class Robot extends TimedRobot {
 
   private Command m_autoCommand;
-    
-
   private RobotContainer m_robotContainer;
 
   public enum RobotId {
