9a69224cc7a62d280ecdd1bc629e73a1e0b3522d
omega <62168528+omegaladon@users.noreply.github.com>
2023-02-25 16:04:51 -0800

    Rework TestVisionDistance

--- src/main/java/frc/robot/commands/vision/TestVisionDistance.java
+++ src/main/java/frc/robot/commands/vision/TestVisionDistance.java
@@ -7,28 +7,27 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.util.Vision;
 
+/**
+ * Gathers data on the distance limits of the camera used for vision.
+ */
 public class TestVisionDistance extends CommandBase{
-  private Drivetrain m_drive;
-  private Vision m_vision;
+  private final Drivetrain m_drive;
+  private final Vision m_vision;
   private Translation2d m_visionStartTranslation, m_driveStartTranslation;
   private Pose2d m_currentPose = null;
-  private int m_endCounter = 0;
-  private int m_printCounter=0;
+
   private double m_speed;
 
-  // How many rio cycles it has to not see anything to end the command
-  private static final int endDelay = 5;
+  private final Timer m_endTimer = new Timer();
+  private final Timer m_printTimer = new Timer();
 
-  // How many rio cycles it will wait between prints
-  private static final int printDelay = 50;
+  // TODO: Should these values be in a constant file?
+  // How many seconds of not seeing april tag before ending the command
+  private static final int kEndDelay = 5;
 
-  /**
-   * A command that moves and prints out distances
-   * It will stop when it can't see an april tag
-   * @param speed What speed to move at
-   * @param drive The drivetrain
-   * @param vision The vision
-   */
+  // How many seconds between each data print
+  private static final int kPrintDelay = 3;
+
   public TestVisionDistance(double speed, Drivetrain drive, Vision vision){
     addRequirements(drive);
     m_drive = drive;
@@ -36,35 +35,33 @@ public class TestVisionDistance extends CommandBase{
     m_vision = vision;
   }
 
-  /**
-   * Initializes the command
-   */
   @Override
   public void initialize(){
+
+    m_endTimer.reset();
+    m_printTimer.restart();
+
     m_drive.enableVision(false);
+
     m_currentPose = m_vision.getPose2d(null, m_drive.getPose());
     m_visionStartTranslation = m_currentPose.getTranslation();
     m_driveStartTranslation = m_drive.getPose().getTranslation();
   }
 
-  /**
-   * Moves the robot and prints the distances
-   * If it can't see an April tag, it increases m_endCounter
-   */
   @Override
   public void execute(){
     m_drive.drive(m_speed, 0, 0, false);
     Pose2d newestPose = m_vision.getPose2d(m_currentPose, m_drive.getPose());
 
-    // If the camera can't see the apriltag
-    if(newestPose != null){
+    // If the camera can see the apriltag
+    if (newestPose != null){
       //update current pose
       m_currentPose = newestPose;
-      // reset command end counter
-      m_endCounter = 0;
-      // print every few cycle
-      m_printCounter++;
-      if (m_printCounter % printDelay == 0) {
+      // reset the timer
+      m_endTimer.reset();
+      // If kPrintDelay seconds have passed, print the data
+      // TODO: Consider using shuffleboard instead of printing
+      if (m_printTimer.advanceIfElapsed(kPrintDelay)) {
         double driveDistance = m_drive.getPose().getTranslation().getDistance(m_driveStartTranslation);
         double visionDistance = m_currentPose.getTranslation().getDistance(m_visionStartTranslation);
         System.out.printf("\nEncoder distance: %.4f\nVision distance: %.4f\n",
@@ -73,26 +70,19 @@ public class TestVisionDistance extends CommandBase{
           visionDistance - driveDistance, (visionDistance - driveDistance) / driveDistance * 100);
       } 
     } else {
-      m_endCounter++;
+      m_endTimer.start();
     }
   }
 
-  /**
-   * Stops the robot
-   * @param interrupted If the command is interrupted
-  */
   @Override
   public void end(boolean interrupted){
     m_drive.enableVision(true);
     m_drive.stop();
   }
 
-  /**
-   * Returns if the command is finished
-   * @return If m_endCounter is greater than endDelay
-   */
   @Override
   public boolean isFinished(){
-    return m_endCounter >= endDelay;
+    return m_endTimer.hasElapsed(kEndDelay);
   }
+
 }
