8a3235bffb4af41a5df2016dbe7694df2f5da91d
Pennedrose <tamg6397@lgsstudent.org>
2023-02-26 13:50:11 -0800

    auto routine without path planner

--- src/main/java/frc/robot/commands/auto/AutoPathNoPlan.java
+++ src/main/java/frc/robot/commands/auto/AutoPathNoPlan.java
@@ -3,29 +3,39 @@ package frc.robot.commands.auto;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.GoToPose;
 import frc.robot.subsystems.Drivetrain;
 
 public class AutoPathNoPlan extends SequentialCommandGroup{
 
     private Drivetrain m_drive;
-    private Pose2d m_pose, startPose;
-    private Rotation2d rot;
+    private Pose2d midPose, startPose, endPose, initialPose;
+    private Rotation2d rot, startRot;
 
     public AutoPathNoPlan(Drivetrain drive)  {
         m_drive = drive;
 
-        rot = new Rotation2d(0);
+        startRot = new Rotation2d(0);
+        rot = new Rotation2d(Math.PI);
 
-        m_pose = new Pose2d(4.04, 0, rot);
-        startPose = new Pose2d();
+        midPose = new Pose2d(2.54, 0, rot);
+        startPose = new Pose2d(2.54, 0, startRot);
+        endPose = new Pose2d(5.5, 0, rot);
+        initialPose = new Pose2d(-.5,0, startRot);//it is -0.5 because for some reason when it is 0 it doesnt come back all the way
 
-        
         addCommands(
             //deposit
-            new GoToPose(m_drive, m_pose), //it goes to the place we want it to
+            //new GoToPose(m_drive, startPose),
+            //new WaitCommand(2),
+            //new GoToPose(m_drive, midPose),
+            //new WaitCommand(2),
+            new GoToPose(m_drive, endPose), //it goes to the place we want it to
+            new WaitCommand(1),
+            new GoToPose(m_drive, initialPose)
+            //new WaitCommand(2),
             //intake
-            new GoToPose(m_drive, startPose)// it goes back to start position
+            //new GoToPose(m_drive, startPose)// it goes back to start position
 
         );
     }
