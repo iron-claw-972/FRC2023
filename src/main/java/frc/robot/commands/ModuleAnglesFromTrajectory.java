07e3d251efa736b112fcbea908cf53ba6ff9c93e
omega <62168528+omegaladon@users.noreply.github.com>
2023-02-26 18:17:51 -0800

    Update ModuleAnglesFromTrajectory.java

--- src/main/java/frc/robot/commands/ModuleAnglesFromTrajectory.java
+++ src/main/java/frc/robot/commands/ModuleAnglesFromTrajectory.java
@@ -1,5 +1,8 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -21,29 +24,39 @@ public class ModuleAnglesFromTrajectory extends CommandBase {
   }
   
   @Override
-  public void execute() {
-    m_drive.setAllOptimize(true);
-    m_drive.setModuleStates(new SwerveModuleState[] {
-      new SwerveModuleState(0, m_trajectory.getInitialPose().getRotation()),
-      new SwerveModuleState(0, m_trajectory.getInitialPose().getRotation()),
-      new SwerveModuleState(0, m_trajectory.getInitialPose().getRotation()),
-      new SwerveModuleState(0, m_trajectory.getInitialPose().getRotation())
+  public void initialize() {
+
+    // 0.01 is the time between trajectory samples, in seconds
+    // Can be replaced for any small number, but it should be the same as the time between all uses
+    double time = 0.01;
+
+    m_drive.enableStateDeadband(false);
+
+    Pose2d initialPose = m_trajectory.getInitialPose();
+    Pose2d nextPose = m_trajectory.sample(time).poseMeters;
+
+    double xVelo = (nextPose.getX() - initialPose.getX()) / time;
+    double yVelo = (nextPose.getY() - initialPose.getY()) / time;
+    double angularVelo = (nextPose.getRotation().getRadians() - initialPose.getRotation().getRadians()) / time;
+
+    ChassisSpeeds chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(xVelo, yVelo, angularVelo, initialPose.getRotation());
+
+    m_drive.setModuleStates(new SwerveModuleState[]{
+      new SwerveModuleState(0, Rotation2d.fromRadians(chassisSpeeds.omegaRadiansPerSecond)),
+      new SwerveModuleState(0, Rotation2d.fromRadians(chassisSpeeds.omegaRadiansPerSecond)),
+      new SwerveModuleState(0, Rotation2d.fromRadians(chassisSpeeds.omegaRadiansPerSecond)),
+      new SwerveModuleState(0, Rotation2d.fromRadians(chassisSpeeds.omegaRadiansPerSecond))
     });
   }
 
   @Override
   public boolean isFinished() {
-    return (
-      m_drive.m_modules[0].getSteerAngle() == m_trajectory.getInitialPose().getRotation().getRadians() &&
-      m_drive.m_modules[1].getSteerAngle() == m_trajectory.getInitialPose().getRotation().getRadians() &&
-      m_drive.m_modules[2].getSteerAngle() == m_trajectory.getInitialPose().getRotation().getRadians() &&
-      m_drive.m_modules[3].getSteerAngle() == m_trajectory.getInitialPose().getRotation().getRadians()
-    );
+    return true;
   }
-  
+
   @Override
   public void end(boolean interrupted) {
-    m_drive.stop();
+    m_drive.enableStateDeadband(true);
   }
     
 }
