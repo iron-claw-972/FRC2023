e019e8327b093cb78e256b6aec57ddf24bee989a
lakshag32 <lakshag32@gmail.com>
2023-02-27 17:07:58 -0800

    add functionality to calculate elevator extension from height

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -17,21 +17,24 @@ public class ElevatorConstants {
   // Game constants
   /** Middle Node height (in meters) */
   public static final double kMiddleNodeHeightCone = Units.inchesToMeters(24.5);
-  public static final double kTopNodeHeightCone = Units.inchesToMeters(46);
+  public static final double kTopNodeHeightCone = Units.inchesToMeters(34.5);
+  
   public static final double kMiddleNodeHeightCube = Units.inchesToMeters(23.5);
   public static final double kTopNodeHeightCube = Units.inchesToMeters(46);
+  
   public static final double kShelfIntakeHeight = Units.inchesToMeters(37.375);
 
   /** elevator travel distance in meters**/
+  public static final double kMiddleNodeHeightConeExtension = heightToElevatorExtension(kMiddleNodeHeightCone);//0.65;
+  public static final double kTopNodeHeightConeExtension = heightToElevatorExtension(kTopNodeHeightCone); // 1.2; 
+  
+  public static final double kMiddleNodeHeightCubeExtension = heightToElevatorExtension(kMiddleNodeHeightCube);
+  public static final double kTopNodeHeightCubeExtension = heightToElevatorExtension(kTopNodeHeightCube);  
+  
+  public static final double kHybridNodeOuttakeExtension = 0;
+  public static final double kGroundIntakeExtension = 0;
 
-  public static final double kMiddleNodeHeightConeExtension = 0.65 ;
-  public static final double kTopNodeHeightConeExtension = 1.2; 
-  public static final double kHybridNodeOuttakeExtension = 0.14;
-  public static final double kGroundIntakeExtension = 0.14;
-  public static final double kMiddleNodeHeightCubeExtension = 0.1;
-  public static final double kTopNodeHeightCubeExtension = 0.1;
-  public static final double kShelfIntakeHeightExtension = 0.1;
-
+  public static final double kShelfIntakeHeightExtension = heightToElevatorExtension(kShelfIntakeHeight); 
 
   public static final double kMaxExtension = 1.3; 
   public static final double kMinExtension = 0.0; 
@@ -44,6 +47,12 @@ public class ElevatorConstants {
   public static final double kCalibrationPower = 0.2; 
   /** Normal full power level for the elevator motor. */
   public static final double kPowerLimit = 0.3; //1
+
+
+  public static double heightToElevatorExtension(double height){
+    double extension = Units.inchesToMeters(height-10.5)/Math.sin(Units.degreesToRadians(55));
+    return extension; 
+  }
 
 }
 
