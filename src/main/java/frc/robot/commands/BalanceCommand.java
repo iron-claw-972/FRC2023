23765e2c8f925d15cdb28ea71341de7a7bb0fe1c
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2023-02-28 15:50:07 -0800

    Formatting and moveing of features to be used allways in BlanceCommand

--- src/main/java/frc/robot/commands/BalanceCommand.java
+++ src/main/java/frc/robot/commands/BalanceCommand.java
@@ -8,70 +8,73 @@ import frc.robot.constants.swerve.DriveConstants;
 import frc.robot.subsystems.Drivetrain;
 
 public class BalanceCommand extends CommandBase {
-
-    private Drivetrain m_drive;
-    private final PIDController m_pid = new PIDController(
-        DriveConstants.kBalanceP, DriveConstants.kBalanceI, DriveConstants.kBalanceD);
+  
+  private Drivetrain m_drive;
+  private final PIDController m_pid = new PIDController(
+    DriveConstants.kBalanceP, 
+    DriveConstants.kBalanceI, 
+    DriveConstants.kBalanceD
+  );
+  
+  
+  private double m_currentAngle, m_output;
+  
+  private boolean m_usePitch;
+  
+  private int m_inverted;
+  
+  Timer m_timer = new Timer();
+  
+  public BalanceCommand(Drivetrain drive) {
+    m_drive = drive;
+    m_pid.setTolerance(DriveConstants.kBalanceTolerance);
+    addRequirements(drive);
+  }
+  
+  @Override
+  public void initialize() {
     
-
-    private double m_currentAngle, m_output;
-
-    private boolean m_usePitch;
-
-    private int m_inverted;
-
-    Timer timer = new Timer();
-
-    public BalanceCommand(Drivetrain drive) {
-        m_drive = drive;
-        m_pid.setTolerance(DriveConstants.kBalanceTolerance);
-        addRequirements(drive);
-    }
-
-    @Override
-    public void initialize() {
-        
-        m_inverted = 1;
-
-        m_pid.setSetpoint(0);
-        if(Math.abs(Math.PI/2 - Math.abs(m_drive.getAngleHeading())) > Math.PI/4)  { //Determines whether to use roll or pitch
-            m_usePitch = false;
-        }
-        else  {
-            m_usePitch = true;
-        }
-        if(m_drive.getAngleHeading() < 0)  {
-            m_inverted = -1;
-        }
-    }
-
-    @Override
-    public void execute() {
-        timer.start();
-
-        m_currentAngle = m_drive.getPitch();
-        m_output = MathUtil.clamp(m_pid.calculate(m_currentAngle), -DriveConstants.kBalanceMaxOutput, DriveConstants.kBalanceMaxOutput);
+    m_inverted = 1;
     
-        if(m_usePitch) {
-            m_drive.driveHeading(-m_output, 0, m_inverted*Math.PI/2, true);
-        }
-        else {
-            m_drive.driveHeading(-m_output, 0, 0, true);
-            if (timer.get() >= 0 && timer.get() <= DriveConstants.kBalanceTime)
-            {
-                m_drive.stop();
-                timer.reset();
-            }
-        }
+    m_pid.setSetpoint(0);
+    if(Math.abs(Math.PI/2 - Math.abs(m_drive.getAngleHeading())) > Math.PI/4)  { //Determines whether to use roll or pitch
+      m_usePitch = false;
     }
-
-    @Override
-    public boolean isFinished() {
-        return m_pid.atSetpoint();
+    else  {
+      m_usePitch = true;
     }
-
-    @Override
-    public void end(boolean interrupted) {
-        m_drive.stop();
+    if(m_drive.getAngleHeading() < 0)  {
+      m_inverted = -1;
     }
+  }
+  
+  @Override
+  public void execute() {
+    m_timer.start();
+    
+    m_currentAngle = m_drive.getPitch();
+    m_output = MathUtil.clamp(m_pid.calculate(m_currentAngle), -DriveConstants.kBalanceMaxOutput, DriveConstants.kBalanceMaxOutput);
+    
+    if(m_usePitch) {
+      m_drive.driveHeading(-m_output, 0, m_inverted*Math.PI/2, true);
+    }
+    else {
+      m_drive.driveHeading(-m_output, 0, 0, true);
+    }
+    
+    if (m_timer.get() >= 0 && m_timer.get() <= DriveConstants.kBalanceTime) {
+      m_drive.stop();
+      m_timer.reset();
+    }
+  }
+  
+  @Override
+  public boolean isFinished() {
+    return m_pid.atSetpoint();
+  }
+  
+  @Override
+  public void end(boolean interrupted) {
+    m_drive.stop();
+  }
 }   
