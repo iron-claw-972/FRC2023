fa7712e35917a060d5634e7517ff0934293f4817
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2023-02-27 17:27:06 -0800

    remove acuracy test in zero wheels

--- src/main/java/frc/robot/commands/AlignWheelsToZero.java
+++ src/main/java/frc/robot/commands/AlignWheelsToZero.java
@@ -3,10 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.constants.TestConstants;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Module;
-import frc.robot.util.TimeAccuracyTest;
 
 /**
  * Attempts to set all four modules to a constant angle. Determines if the modules are able to reach the angle requested in a certain time.
@@ -14,7 +11,6 @@ import frc.robot.util.TimeAccuracyTest;
 public class AlignWheelsToZero extends CommandBase{
   
   private final Drivetrain m_drive;
-  private TimeAccuracyTest m_timeAccuracyTests[]  = new TimeAccuracyTest[4];
 
   public AlignWheelsToZero(Drivetrain drive) {
     m_drive = drive;
@@ -25,15 +21,6 @@ public class AlignWheelsToZero extends CommandBase{
   public void initialize() {
     m_drive.setAllOptimize(false);
     m_drive.enableStateDeadband(false);
-    for (int i = 0; i < 4; i++){
-      Module module = m_drive.m_modules[i];
-      m_timeAccuracyTests[i] = new TimeAccuracyTest(
-        () -> module.getDriveVelocityError(),
-        () -> m_drive.getRequestedSteerVelocityEntry().getDouble(0),
-        TestConstants.kSteerAngleError,
-        TestConstants.kSteerAngleTimeError
-      );
-    }
   }
   
   @Override
