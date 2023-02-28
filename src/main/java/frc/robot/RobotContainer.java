81cc10290775c0bd7661933b3c9f8d2dce0def23
Pennedrose <tamg6397@lgsstudent.org>
2023-02-27 16:50:48 -0800

    called function in robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -130,6 +130,7 @@ public class RobotContainer {
     m_drive.setupModulesShuffleboard();
     m_vision.setupVisionShuffleboard();
     m_driver.setupShuffleboard();
+    m_vision.logging();
     
     addTestCommands();
 
