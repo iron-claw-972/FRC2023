628fd2a78f5bc7030ab4aeaef2e60fd5460eef8c
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2023-02-28 23:17:05 -0800

    remove outtdated comments

--- src/main/java/frc/robot/constants/ArmConstants.java
+++ src/main/java/frc/robot/constants/ArmConstants.java
@@ -5,13 +5,9 @@ package frc.robot.constants;
  */
 public class ArmConstants {
 
-  // TODO: arm ids - TBD (fake using 5 -- 1-4 have been taken)
   public static final int kMotorId = 5;
   public static final int kAbsEncoderId = 7; 
 
-
-
-  // TODO: PID values - TBD (fake values for now)
   public static final double kP = 8;
   public static final double kI = 0.0;
   public static final double kD = 0.0;
@@ -19,7 +15,7 @@ public class ArmConstants {
   public static final double kMinMotorPower = -0.1;
   public static final double kMaxMotorPower = 0.1;
 
-  // TODO: distance values - TBD (fake values for now)
+  // TODO: distance values - TBD
   public static final double kStowedAbsEncoderPos = -0.466;
   public static final double kShelfPositionAbsEncoderPos = -0.2;
   public static final double kBottomNodePositionAbsEncoderPos = -0.2;
