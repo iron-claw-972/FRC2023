beb29dda661ebdd61f57cbf02e13c57dc6b4ec78
AnthonyFurman <fura5196@lgsstudent.org>
2023-03-04 17:00:51 -0800

    revert

--- src/main/java/frc/robot/commands/GoToNode.java
+++ src/main/java/frc/robot/commands/GoToNode.java
@@ -1,6 +1,8 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.controls.Operator;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.util.Node;
 
