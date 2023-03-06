ef9d2f51648294c7ea98cac7226b946a5c938a11
GLRoylance <GLRoylance@gmail.com>
2023-03-06 12:53:43 -0800

    fix angle

--- src/main/java/frc/robot/util/Mechanism.java
+++ src/main/java/frc/robot/util/Mechanism.java
@@ -33,29 +33,33 @@ public class Mechanism {
     //   make ground level be 2 inches from the bottom.
     MechanismRoot2d m_root = m_mech2d.getRoot("ChargedUp", 18, 2);
 
-    // how far away the grid is from the root
+    /** how far away the grid is from the root (robot center) (inches) */
     MechanismLigament2d m_distGrid;
 
-    // set the length of this element for the length of the elevator
+    /** this element is the length of the elevator (inches) */
     MechanismLigament2d m_elevator;
 
-    // the FourBar
+    /** the FourBar linkage that pivots. Set the angle of this linkage. */
     MechanismLigament2d m_fb2;
 
-    // the intake
+    /** the intake linkage that pivots off the FourBar */
     MechanismLigament2d m_intake;
 
     private Mechanism() {
+        // define colors for the linkages
         Color8Bit colorSpace = new Color8Bit(0, 0, 0);
         Color8Bit colorNode = new Color8Bit(0, 255, 0);
         Color8Bit colorElevator = new Color8Bit(255, 128, 128);
         Color8Bit colorFourBar = new Color8Bit(255, 255, 0);
         Color8Bit colorIntake = new Color8Bit(128, 128, 255);
         Color8Bit colorShelf = new Color8Bit(255, 0, 0);
+        Color8Bit colorDist =  new Color8Bit(128, 128, 128);
 
         // Build the grid
-        // distance of the grid from the root
-        m_distGrid = new MechanismLigament2d("distGrid", 18, 0, 1.0, new Color8Bit(128, 128, 128));
+
+        // the entire grid is built relative to m_distGrid so the grid can be moved relative to the robot
+        // distance of the grid from the root robot center (root)
+        m_distGrid = new MechanismLigament2d("distGrid", 18, 0, 1.0, colorDist);
         m_root.append(m_distGrid);
 
         // front of grid is 5 inches high
@@ -101,10 +105,13 @@ public class Mechanism {
 
 
         // build the robot.
-        // the root should be the center of the robot, but it is not right now
+        // the root should be the center of the robot
         // the deck of the robot should be off the ground.
         // The robot should be described in more detail.
+
         // The Bumpers
+        // the robot is 26 inches by 26 inches.
+        // Ignores bumper thickness for now. Should set width and color.
         MechanismLigament2d bumper1 = new MechanismLigament2d("bumper1", 13, 0);
         MechanismLigament2d bumper2 = new MechanismLigament2d("bumper2", 13, 180);
         m_root.append(bumper1);
@@ -112,23 +119,30 @@ public class Mechanism {
 
         // make the elevator
         // elevator is 10 inches high and 12 inches back
+        // go back 12 inches
         MechanismLigament2d spaceElevatorBack = new MechanismLigament2d("spaceEL", 12, 180, 0.2, colorSpace);
         m_root.append(spaceElevatorBack);
+        // go up 10 inches
         MechanismLigament2d spaceElevatorUp = new MechanismLigament2d("elUp", 10, -90, 0.25, colorSpace);
         spaceElevatorBack.append(spaceElevatorUp);
+        // the elevator is slanted at 55 degrees.
         m_elevator = new MechanismLigament2d("elevator", 50, 55.0-90.0, 3.0, colorElevator);
         spaceElevatorUp.append(m_elevator);
 
         // make the four bar (fake for now)
+        // fb1 is portion that is horizontal and extends out from elevator carriage
         MechanismLigament2d fb1 = new MechanismLigament2d("fb1", 13.5, -55, 3.0, colorFourBar);
         m_elevator.append(fb1);
-        // moving part of the FourBar
+        // m_fb2 is moving part of the FourBar (angle is read by the absolute encoder)
         m_fb2 = new MechanismLigament2d("fb2", 9.5, 150.0, 3.0, colorFourBar);
         fb1.append(m_fb2);
 
-        // make the intake
+        // make the intake. Its angle will be a function of the m_fb2 angle
         m_intake = new MechanismLigament2d("intake", 13.5, -150.0 * (32.0/48.0), 4.0, colorIntake);
         m_fb2.append(m_intake);
+
+        // set the four bar and intake angles to be consistent
+        setFourBarAngle(144.0);
 
         // put the Mechanism2D on the dashboard
         SmartDashboard.putData("Mech2d", m_mech2d);
@@ -162,12 +176,13 @@ public class Mechanism {
         // Say bumper thickness is 3.5 inches (slight compression)
         distInches = MathUtil.clamp(distInches, 16.5, 48.0);
         
+        // set the display distance
         m_distGrid.setLength(distInches);
     }
 
     /**
      * Set the slant length of the elevator.
-     * @param height
+     * @param height slant height in meters
      */
     public void setElevatorHeight(double height) {
         m_elevator.setLength(Units.metersToInches(height) + 4.0);
@@ -175,14 +190,19 @@ public class Mechanism {
 
     /**
      * Placeholder for setting FourBar state.
+     * The absolute encoder before competition read -0.4 to 0.0 revolutions.
+     * That is -0.4 * 360 = -144 degrees to 0 degrees.
+     * When m_fb2 is is pointing a bit down (20 degrees?), the intake angle is level.
      * @param angle
      */
     public void setFourBarAngle(double angle) {
-        // restrict angle to -170, 0
-        double ang = MathUtil.clamp(angle, 0, 170);
+        // restrict angle to actual range
+        double ang = MathUtil.clamp(angle, 0.0, 144.0);
 
-        m_fb2.setAngle(ang);
-        // 32 tooth to  48 tooth
-        m_intake.setAngle(-ang * (32.0 / 48.0));
+        // an angle of 0 is actually down 20 degrees
+        m_fb2.setAngle(ang - 20.0);
+
+        // 32 tooth to 48 tooth
+        m_intake.setAngle(-ang * (32.0 / 48.0) + 20.0);
     }
 }
