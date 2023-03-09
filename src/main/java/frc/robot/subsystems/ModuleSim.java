0ab0b37c236b923c6359ba269c88130faec4a4f7
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2023-03-08 18:14:50 -0800

    compling fixes

--- src/main/java/frc/robot/subsystems/ModuleSim.java
+++ src/main/java/frc/robot/subsystems/ModuleSim.java
@@ -3,9 +3,9 @@ package frc.robot.subsystems;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.constants.Constants;
-import frc.robot.constants.ModuleConstants;
+import frc.robot.constants.swerve.ModuleConstants;
 
 /**
  * Swerve module for drivetrain to be used inside of simulation.
@@ -18,29 +18,8 @@ public class ModuleSim extends Module {
   private double m_currentDrivePositionMeters = 0;
   private double m_currentSpeed = 0;
 
-  public ModuleSim(
-    ModuleConstants moduleConstants
-  ) {
-    this(
-      moduleConstants.getDrivePort(),
-      moduleConstants.getSteerPort(),
-      moduleConstants.getEncoderPort(),
-      moduleConstants.getSteerOffset(),
-      moduleConstants.getDriveKS(),
-      moduleConstants.getDriveKV()
-    );
-  }
-
-  public ModuleSim(
-    int driveMotorPort,
-    int steerMotorPort,
-    int encoderPort,
-    double encoderOffset,
-    double feedforwardKS,
-    double feedforwardKV
-  ) {
-
-    super(driveMotorPort, steerMotorPort, encoderPort, encoderOffset, feedforwardKS, feedforwardKV);
+  public ModuleSim(ModuleConstants moduleConstants, ShuffleboardTab swerveTab) {
+    super(moduleConstants, swerveTab);
   }
 
   /**
@@ -64,9 +43,10 @@ public class ModuleSim extends Module {
    * Sets the desired state for the module.
    *
    * @param desiredState Desired state with speed and angle.
+   * @param isOpenLoop whether to use closed/open loop control for drive velocity
    */
   @Override
-  public void setDesiredState(SwerveModuleState desiredState) {
+  public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {
     if (Math.abs(desiredState.speedMetersPerSecond) < 0.001) {
       m_currentSpeed = 0;
       return;
@@ -82,8 +62,8 @@ public class ModuleSim extends Module {
    * Gets the simulated angle of the module.
    */
   @Override
-  public double getAngle() {
-    return m_currentSteerPositionRad;
+  public Rotation2d getAngle() {
+    return new Rotation2d(m_currentSteerPositionRad);
   }
 
   @Override
