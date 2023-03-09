3852a0bbc21d0c948ee7c4e3cd050dedfa1b2286
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2023-03-08 17:30:57 -0800

    remove unnecessary import

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.constants.Constants;
 import frc.robot.util.LogManager;
 
