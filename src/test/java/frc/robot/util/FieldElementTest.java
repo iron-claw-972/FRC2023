5d10072b38433f85a34015e5a5bebe915e1d773a
GLRoylance <GLRoylance@gmail.com>
2023-03-11 12:19:24 -0800

    remove unused import

--- src/test/java/frc/robot/util/FieldElementTest.java
+++ src/test/java/frc/robot/util/FieldElementTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import edu.wpi.first.apriltag.AprilTag;
