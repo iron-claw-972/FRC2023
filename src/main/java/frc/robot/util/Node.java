310b9f339f5359666e260e02d9473c882bf53ccc
omega <62168528+omegaladon@users.noreply.github.com>
2023-03-10 17:42:58 -0800

    Optimize imports

--- src/main/java/frc/robot/util/Node.java
+++ src/main/java/frc/robot/util/Node.java
@@ -1,9 +1,5 @@
 package frc.robot.util;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
