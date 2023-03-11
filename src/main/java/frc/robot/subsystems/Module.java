c2b105ad86b0069ccd1b9d303c89acc3e13d19a3
AnthonyFurman <fura5196@lgsstudent.org>
2023-03-10 16:00:03 -0800

    remove m_lastAngle

--- src/main/java/frc/robot/subsystems/Module.java
+++ src/main/java/frc/robot/subsystems/Module.java
@@ -25,7 +25,6 @@ public class Module {
 
   private final int m_moduleIndex;
   private Rotation2d m_angleOffset;
-  private Rotation2d m_lastAngle;
 
   private final String m_moduleAbbr;
 
@@ -60,8 +59,7 @@ public class Module {
     m_driveMotor = new WPI_TalonFX(moduleConstants.getDrivePort(), DriveConstants.kDriveMotorCAN);
     configDriveMotor();
 
-    m_lastAngle = getState().angle;
-    setDesiredState(new SwerveModuleState(0, m_lastAngle), false);
+    setDesiredState(new SwerveModuleState(0, getAngle()), false);
 
     setupShuffleboard();
   }
