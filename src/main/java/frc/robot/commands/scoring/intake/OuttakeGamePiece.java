a1d52b26d3eff13abdb6f0a5a1ed3ef5ace8dc95
omega <62168528+omegaladon@users.noreply.github.com>
2023-03-11 16:31:19 -0800

    Finish intake / outtake

--- src/main/java/frc/robot/commands/scoring/intake/OuttakeGamePiece.java
+++ src/main/java/frc/robot/commands/scoring/intake/OuttakeGamePiece.java
@@ -1,44 +1,46 @@
 package frc.robot.commands.scoring.intake;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.constants.IntakeConstants;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Intake.IntakeMode;
 import frc.robot.subsystems.Intake.IntakeObject;
 
-public class IntakeGamePiece extends CommandBase {
+public class OuttakeGamePiece extends CommandBase {
 
   private final Intake m_intake; 
-  private final IntakeObject m_type;
+  private final Timer m_timer;
 
   /**
    * Spins the intake until the game piece is inside the intake.
    * @param intake the intake subsystem
    */
-  public IntakeGamePiece(Intake intake, IntakeObject type) {
+  public OuttakeGamePiece(Intake intake) {
     m_intake = intake; 
-    m_type = type;
+    m_timer = new Timer();
     addRequirements(m_intake);
   }
 
   @Override
   public void initialize() {
-    if (m_type == IntakeObject.CUBE) {
-      m_intake.setIntakeMode(IntakeMode.INTAKE_CUBE);
-    } else if (m_type == IntakeObject.CONE) {
-      m_intake.setIntakeMode(IntakeMode.INTAKE_CONE);
+    if (m_intake.getObject() == IntakeObject.CUBE) {
+      m_intake.setIntakeMode(IntakeMode.OUTTAKE_CUBE);
+    } else if (m_intake.getObject() == IntakeObject.CONE) {
+      m_intake.setIntakeMode(IntakeMode.OUTTAKE_CONE);
     }
+    m_timer.start();
   }
 
   @Override
   public void end(boolean interrupted) {
-    m_intake.setObject(m_type);
+    m_intake.setObject(IntakeObject.NONE);
     m_intake.setIntakeMode(IntakeMode.DISABLED);
   }
   
   @Override
   public boolean isFinished() {
-    if (m_intake.getIntakeMotor().getStatorCurrent() >= IntakeConstants.kIntakeCurrentStopPoint) {
+    if (m_timer.advanceIfElapsed(IntakeConstants.kOuttakeTime)) {
       return true;
     }
     return false;
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,16 +8,14 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.Rev2mDistanceSensor;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.Rev2mDistanceSensor.Port;
 
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.ArmConstants;
 import frc.robot.constants.Constants;
 import frc.robot.constants.IntakeConstants;
 import frc.robot.util.LogManager;
@@ -26,48 +24,101 @@ import frc.robot.util.MotorFactory;
 
 public class Intake extends SubsystemBase {
 
-  private final WPI_TalonFX m_intakeMotor;
-  private IntakeMode m_mode; 
+  public enum IntakeMode {
+    INTAKE_CUBE, OUTTAKE_CUBE, INTAKE_CONE, OUTTAKE_CONE, DISABLED
+  }
 
+  public enum IntakeObject {
+    CUBE, CONE, NONE
+  }
+
+  private final WPI_TalonFX m_intakeMotor;
+  private final ShuffleboardTab m_intakeTab;
+
+  private IntakeMode m_mode;
+  private IntakeObject m_object;
+
+  private double m_power;
 
   public Intake(ShuffleboardTab intakeTab) {
     m_intakeMotor = MotorFactory.createTalonFX(IntakeConstants.kIntakeMotorId, Constants.kCanivoreCAN);
     configMotors();
+    
+    m_power = 0;
     m_mode = IntakeMode.DISABLED;
+    // During auto, this doesn't really matter, so we can just set it to NONE
+    m_object = IntakeObject.NONE;
+    m_intakeTab = intakeTab;
+
+    setupShuffleboard();
+  
+    if (Constants.kLogging) {
+      LogManager.addDouble("Intake Motor Current", () -> m_intakeMotor.getStatorCurrent());
+      LogManager.addDouble("Intake Power", () -> m_power);
+    }
 
   }
-
 
   private void configMotors() {
-    //m_intakeMotor.setInverted(true); 
-  }
-
-  public enum IntakeMode {
-    INTAKE_CUBE, OUTTAKE_CUBE, INTAKE_CONE, OUTTAKE_CONE, DISABLED
+    m_intakeMotor.setNeutralMode(IntakeConstants.kNeutralMode);
   }
 
   public void setIntakeMode(IntakeMode mode){
     m_mode = mode; 
   }
 
-  public void setMotorPower(double power){
+  private void setMotorPower(double power){
     m_intakeMotor.set(power); 
   }
 
   @Override
   public void periodic() {
-    switch(m_mode){
-      case INTAKE_CUBE: 
-        setMotorPower(IntakeConstants.kIntakeCubePower); 
-      case OUTTAKE_CUBE: 
-        setMotorPower(IntakeConstants.kOuttakeCubePower);   
-      case INTAKE_CONE: 
-        setMotorPower(IntakeConstants.kIntakeConePower);        
-      case OUTTAKE_CONE: 
-        setMotorPower(IntakeConstants.kOuttakeConePower);        
-      case DISABLED: 
-        setMotorPower(IntakeConstants.kStopPower);       
-      }
-    }
 
+    switch (m_mode) {
+      case INTAKE_CUBE: 
+        m_power = IntakeConstants.kIntakeCubePower;
+      case OUTTAKE_CUBE: 
+        m_power = IntakeConstants.kOuttakeCubePower;   
+      case INTAKE_CONE: 
+        m_power = IntakeConstants.kIntakeConePower;        
+      case OUTTAKE_CONE: 
+        m_power = IntakeConstants.kOuttakeConePower;        
+      case DISABLED: 
+        m_power = IntakeConstants.kStopPower;       
+    }
+    setMotorPower(m_power);
+  }
+
+  private void setupShuffleboard() {
+    m_intakeTab.addString("Intake Mode", () -> m_mode.name());
+
+  }
+  
+  public void updateLogs() {
+    
+  }
+  
+  public boolean hasObject() {
+    return m_object != IntakeObject.NONE;
+  }
+
+  public boolean hasCone() {
+    return m_object == IntakeObject.CONE;
+  }
+
+  public boolean hasCube() {
+    return m_object == IntakeObject.CUBE;
+  }
+
+  public WPI_TalonFX getIntakeMotor() {
+    return m_intakeMotor;
+  }
+
+  public IntakeObject getObject() {
+    return m_object;
+  }
+
+  public void setObject(IntakeObject object) {
+    m_object = object;
+  }
 } 
