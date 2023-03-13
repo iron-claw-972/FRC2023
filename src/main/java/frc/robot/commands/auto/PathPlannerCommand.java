54afb2fc245ccb93cd8a8ae68388eff7852edbc6
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2023-03-13 11:45:56 -0700

    resolve merge error

--- src/main/java/frc/robot/commands/auto/PathPlannerCommand.java
+++ src/main/java/frc/robot/commands/auto/PathPlannerCommand.java
@@ -11,6 +11,7 @@ import com.pathplanner.lib.PathPoint;
 import com.pathplanner.lib.auto.SwerveAutoBuilder;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
