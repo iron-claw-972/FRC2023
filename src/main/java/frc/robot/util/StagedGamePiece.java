41de55beb3dd4edab4bf25e9452b1d46a7f479ee
omega <62168528+omegaladon@users.noreply.github.com>
2023-03-13 10:29:27 -0700

    Update staged game piece

--- src/main/java/frc/robot/util/StagedGamePiece.java
+++ src/main/java/frc/robot/util/StagedGamePiece.java
@@ -1,21 +1,19 @@
 package frc.robot.util;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.constants.FieldConstants;
 
 /**
- * Represents a game piece that is staged on the field before the match starts. The {@link Pose2d} for the game piece is relative to the field origin.
- * The Pose2d will contain the Translation2d of the game piece and the Rotation2d of the game piece. The rotation will be the angle from the center line to the respective alliance color's grid.
+ * Represents a game piece that is staged on the field before the match starts.
  */
 public class StagedGamePiece {
 
   public final Alliance m_alliance;
   public final int m_id;
-  public final Pose2d m_pose;
+  public final Translation2d m_translation;
+  public final GamePieceType m_type;
 
   /**
    * Represents a new staged game piece.
@@ -23,9 +21,14 @@ public class StagedGamePiece {
    * 
    * @param alliance the alliance color 
    * @param id the index of the piece
+   * @param type the type of game piece
    * @throws IllegalArgumentException if id is not between 1 and 4 inclusive
    */
-  public StagedGamePiece(Alliance alliance, int id) {
+  public StagedGamePiece(Alliance alliance, int id, GamePieceType type) {
+
+    if (type == GamePieceType.NONE) {
+      throw new IllegalArgumentException("Game piece type cannot be NONE.");
+    }
 
     if (id < 1 || id > 4) {
       throw new IllegalArgumentException("The id must be between 1 and 4 inclusive.");
@@ -33,6 +36,7 @@ public class StagedGamePiece {
 
     m_alliance = alliance;
     m_id = id;
+    m_type = type;
 
     double x = FieldConstants.kFieldWidth / 2;
     if (m_alliance == Alliance.Blue) {
@@ -44,13 +48,7 @@ public class StagedGamePiece {
     // Values here obtained from game manual.
     double y = Units.feetToMeters(3.25 + ((m_id - 1) * 4));
 
-    Rotation2d rotation = new Rotation2d();
-    if (m_alliance == Alliance.Blue) {
-      rotation = rotation.plus(Rotation2d.fromDegrees(180));
-    }
-
-    m_pose = new Pose2d(new Translation2d(x, y), rotation);
-
+    m_translation = new Translation2d(x, y);
 
   }
   
