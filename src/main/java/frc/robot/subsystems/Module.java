1e0d957346a8f09fe7f8d6c4dfd5cbb4db1bd3e0
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2023-03-15 19:54:54 -0700

    method reordering

--- src/main/java/frc/robot/subsystems/Module.java
+++ src/main/java/frc/robot/subsystems/Module.java
@@ -71,132 +71,12 @@ public class Module extends SubsystemBase {
     
   }
 
-  /**
-   * set module desired state
-   * @param desiredState state to set module to
-   * @param isOpenLoop don't use integrated velocity PID and instead set percentage power for state velocity
-   */
-  public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {
-    /*
-     * This is a custom optimize function, since default WPILib optimize assumes
-     * continuous controller which CTRE and Rev onboard is not
-     */
-    desiredState = m_optimizeStates ? CTREModuleState.optimize(desiredState, getState().angle) : desiredState;
-    m_desiredState = desiredState;
-    setAngle(desiredState);
-    setSpeed(desiredState, isOpenLoop);
-  }
-  
-  /**
-   * sets desired state velocity
-   * @param desiredState desired state with desired velocity
-   * @param isOpenLoop don't use integrated velocity PID and instead set percentage power
-   */
-  private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop) {
-    // TODO: why is open loop even a option check if needed for driver
-    if (isOpenLoop) {
-      double percentOutput = desiredState.speedMetersPerSecond / DriveConstants.kMaxSpeed;
-      m_driveMotor.set(ControlMode.PercentOutput, percentOutput);
-    } else {
-      double velocity = Conversions.MPSToFalcon(
-        desiredState.speedMetersPerSecond, 
-        DriveConstants.kWheelCircumference,
-        DriveConstants.kDriveGearRatio);
-      m_driveMotor.set(ControlMode.Velocity, velocity, DemandType.ArbitraryFeedForward,
-          feedforward.calculate(desiredState.speedMetersPerSecond));
-    }
-  }
-
-  /**
-   * sets desired angle base off desired state. Will not move motor is desired speed is too low.
-   * @param desiredState desired state of swerve module
-   */
-  private void setAngle(SwerveModuleState desiredState) {
-    // Prevent rotating module if desired speed < 1% of max speed. Prevents Jittering.
-    if (m_stateDeadband && (Math.abs(desiredState.speedMetersPerSecond) <= (DriveConstants.kMaxSpeed * 0.01))) {
-      stop();
-      return;
-    }
-
-    setAngle(desiredState.angle);
-  }
-
-  /**
-   * rotates swerve module steer to desired angle
-   * @param rotation2d desired steer angle
-   * @param rotationBlind should the angle be interpreted continuously
-   */
-  public void setAngle(Rotation2d rotation2d, boolean rotationBlind) {
-    double angle = rotation2d.getRadians();
-    if (rotationBlind) angle = MathUtil.inputModulus(
-      angle, 
-      getAngle().getRadians() - Math.PI,
-      getAngle().getRadians() + Math.PI
-    );
-
-    m_desiredState.angle = new Rotation2d(angle);
-
-    m_angleMotor.set(ControlMode.Position, Conversions.degreesToFalcon(
-      rotation2d.getDegrees(), DriveConstants.kAngleGearRatio
-    ));
-  }
-
-  /**
-   * rotates swerve module steer to desired angle without the angle being interpreted continuously
-   * @param rotation2d desired steer angle
-   */
-  public void setAngle(Rotation2d rotation2d) {
-    setAngle(rotation2d, false);
-  }
-  
-  /**
-   * sets state deadband enable status used for not running the module if desired speed is too low
-   * @param enable should state deadband be enabled
-   */
-  public void enableStateDeadband(boolean enabled) {
-    m_stateDeadband = enabled;
-  }
-
-  /**
-   * sets swerve module state optimization enable status used to find least steer angle rotation needed
-   * @param enable should state optimization be enabled
-   */
-  public void setOptimize(boolean enable) {
-    m_optimizeStates = enable;
-  }
-
-  /**
-   * @return index in drivetrain module array
-   */
-  public int getModuleIndex() {
-    return m_moduleIndex;
-  }
-
-  /**
-   * @return get encoder position in meters
-   */
-  public double getDrivePosition(){
-    return Conversions.falconToMeters(
-          m_driveMotor.getSelectedSensorPosition(),
-          DriveConstants.kWheelCircumference,
-          DriveConstants.kDriveGearRatio);
-  }
-
-  /**
-   * @return module steer angle from steer motor's integrated encoder as Rotation2d with 0 being straight forward
-   */
-  public Rotation2d getAngle() {
-    return Rotation2d.fromDegrees( Conversions.falconToDegrees(
-      m_angleMotor.getSelectedSensorPosition(), 
-      DriveConstants.kAngleGearRatio
-    ));
-  }
-
-  /**
-   * @return module steer angle from Cancoder as Rotation2d with 0 being straight forward
-   */
-  public Rotation2d getCANcoder() {
-    return Rotation2d.fromDegrees(m_CANcoder.getAbsolutePosition() - m_angleOffset.getDegrees());
+  private void configCANcoder() {
+    m_CANcoder.configFactoryDefault();
+    m_CANcoder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
+    m_CANcoder.configSensorDirection(DriveConstants.kModuleConstants.canCoderInvert);
+    m_CANcoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
+    m_CANcoder.configFeedbackCoefficient(360 / 4096, "deg", SensorTimeBase.PerSecond);
   }
 
   /**
@@ -208,40 +88,6 @@ public class Module extends SubsystemBase {
       DriveConstants.kAngleGearRatio
     ));
     
-  }
-
-  private void configCANcoder() {
-    m_CANcoder.configFactoryDefault();
-    m_CANcoder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
-    m_CANcoder.configSensorDirection(DriveConstants.kModuleConstants.canCoderInvert);
-    m_CANcoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
-    m_CANcoder.configFeedbackCoefficient(360 / 4096, "deg", SensorTimeBase.PerSecond);
-  }
-
-  private void configAngleMotor() {
-    m_angleMotor.configFactoryDefault();
-    m_angleMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(
-      DriveConstants.kAngleEnableCurrentLimit,
-      DriveConstants.kAngleContinuousCurrentLimit,
-      DriveConstants.kAnglePeakCurrentLimit,
-      DriveConstants.kAnglePeakCurrentDuration
-    ));
-    m_angleMotor.config_kP(0, DriveConstants.kModuleConstants.angleKP);
-    m_angleMotor.config_kI(0, DriveConstants.kModuleConstants.angleKI);
-    m_angleMotor.config_kD(0, DriveConstants.kModuleConstants.angleKD);
-    m_angleMotor.config_kF(0, DriveConstants.kModuleConstants.angleKF);
-    m_angleMotor.setInverted(DriveConstants.kAngleMotorInvert);
-    m_angleMotor.setNeutralMode(DriveConstants.kAngleNeutralMode);
-    m_angleMotor.configVoltageCompSaturation(Constants.kRobotVoltage);
-    m_angleMotor.enableVoltageCompensation(true);
-    resetToAbsolute();
-  }
-
-  /**
-   * @return module steer rotational velocity in radians per second
-   */
-  public double getSteerVelocity() {
-    return Conversions.falconToRPM(m_angleMotor.getSelectedSensorVelocity(), DriveConstants.kAngleGearRatio) * 2 * Math.PI / 60;
   }
 
   private void configDriveMotor() {
@@ -265,6 +111,142 @@ public class Module extends SubsystemBase {
     m_driveMotor.setSelectedSensorPosition(0);
   }
 
+  private void configAngleMotor() {
+    m_angleMotor.configFactoryDefault();
+    m_angleMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(
+      DriveConstants.kAngleEnableCurrentLimit,
+      DriveConstants.kAngleContinuousCurrentLimit,
+      DriveConstants.kAnglePeakCurrentLimit,
+      DriveConstants.kAnglePeakCurrentDuration
+    ));
+    m_angleMotor.config_kP(0, DriveConstants.kModuleConstants.angleKP);
+    m_angleMotor.config_kI(0, DriveConstants.kModuleConstants.angleKI);
+    m_angleMotor.config_kD(0, DriveConstants.kModuleConstants.angleKD);
+    m_angleMotor.config_kF(0, DriveConstants.kModuleConstants.angleKF);
+    m_angleMotor.setInverted(DriveConstants.kAngleMotorInvert);
+    m_angleMotor.setNeutralMode(DriveConstants.kAngleNeutralMode);
+    m_angleMotor.configVoltageCompSaturation(Constants.kRobotVoltage);
+    m_angleMotor.enableVoltageCompensation(true);
+    resetToAbsolute();
+  }
+
+  /**
+   * set module desired state
+   * @param desiredState state to set module to
+   * @param isOpenLoop don't use integrated velocity PID and instead set percentage power for state velocity
+   */
+  public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {
+    /*
+     * This is a custom optimize function, since default WPILib optimize assumes
+     * continuous controller which CTRE and Rev onboard is not
+     */
+    desiredState = m_optimizeStates ? CTREModuleState.optimize(desiredState, getState().angle) : desiredState;
+    m_desiredState = desiredState;
+    setAngle(desiredState);
+    setSpeed(desiredState, isOpenLoop);
+  }
+    /**
+   * sets desired state velocity
+   * @param desiredState desired state with desired velocity
+   * @param isOpenLoop don't use integrated velocity PID and instead set percentage power
+   */
+  private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop) {
+    // TODO: why is open loop even a option check if needed for driver
+    if (isOpenLoop) {
+      double percentOutput = desiredState.speedMetersPerSecond / DriveConstants.kMaxSpeed;
+      m_driveMotor.set(ControlMode.PercentOutput, percentOutput);
+    } else {
+      double velocity = Conversions.MPSToFalcon(
+        desiredState.speedMetersPerSecond, 
+        DriveConstants.kWheelCircumference,
+        DriveConstants.kDriveGearRatio);
+      m_driveMotor.set(ControlMode.Velocity, velocity, DemandType.ArbitraryFeedForward,
+          feedforward.calculate(desiredState.speedMetersPerSecond));
+    }
+  }
+  /**
+   * sets desired angle base off desired state. Will not move motor is desired speed is too low.
+   * @param desiredState desired state of swerve module
+   */
+  private void setAngle(SwerveModuleState desiredState) {
+    // Prevent rotating module if desired speed < 1% of max speed. Prevents Jittering.
+    if (m_stateDeadband && (Math.abs(desiredState.speedMetersPerSecond) <= (DriveConstants.kMaxSpeed * 0.01))) {
+      stop();
+      return;
+    }
+
+    setAngle(desiredState.angle);
+  }
+  /**
+   * rotates swerve module steer to desired angle
+   * @param rotation2d desired steer angle
+   * @param rotationBlind should the angle be interpreted continuously
+   */
+  public void setAngle(Rotation2d rotation2d, boolean rotationBlind) {
+    double angle = rotation2d.getRadians();
+    if (rotationBlind) angle = MathUtil.inputModulus(
+      angle, 
+      getAngle().getRadians() - Math.PI,
+      getAngle().getRadians() + Math.PI
+    );
+
+    m_desiredState.angle = new Rotation2d(angle);
+
+    m_angleMotor.set(ControlMode.Position, Conversions.degreesToFalcon(
+      rotation2d.getDegrees(), DriveConstants.kAngleGearRatio
+    ));
+  }
+  /**
+   * rotates swerve module steer to desired angle without the angle being interpreted continuously
+   * @param rotation2d desired steer angle
+   */
+  public void setAngle(Rotation2d rotation2d) {
+    setAngle(rotation2d, false);
+  }
+  /**
+   * set module motor powers to 0
+   */
+  public void stop() {
+    m_driveMotor.set(0);
+    m_angleMotor.set(0);
+  }
+  /**
+   * sets voltage of drive motor
+   * @param voltage voltage to set drive motor to in volts
+   */
+  public void setDriveVoltage(double voltage) {
+    m_driveMotor.set(ControlMode.PercentOutput, voltage / Constants.kRobotVoltage);
+  }
+    /**
+   * sets voltage of steer motor
+   * @param voltage voltage to set steer motor to in volts
+   */
+  public void setSteerVoltage(double voltage) {
+    m_angleMotor.set(ControlMode.PercentOutput, voltage / Constants.kRobotVoltage);
+  }
+
+  /**
+   * sets state deadband enable status used for not running the module if desired speed is too low
+   * @param enable should state deadband be enabled
+   */
+  public void enableStateDeadband(boolean enabled) {
+    m_stateDeadband = enabled;
+  }
+  /**
+   * sets swerve module state optimization enable status used to find least steer angle rotation needed
+   * @param enable should state optimization be enabled
+   */
+  public void setOptimize(boolean enable) {
+    m_optimizeStates = enable;
+  }
+  
+  /**
+   * @return index in drivetrain module array
+   */
+  public int getModuleIndex() {
+    return m_moduleIndex;
+  }
+  
   /**
    * @return current state of module
    */
@@ -273,7 +255,12 @@ public class Module extends SubsystemBase {
         getDriveVelocity(),
         getAngle());
   }
-
+  /**
+   * @return desired/ target state of module
+   */
+  public SwerveModuleState getDesiredState() {
+    return m_desiredState;
+  }
   /**
    * @return current swerve module position 
    */
@@ -283,37 +270,11 @@ public class Module extends SubsystemBase {
         getAngle());
   }
 
-  private void setupShuffleboard() {
-    if (Constants.kUseTelemetry && RobotBase.isReal()) {
-      m_swerveTab.addDouble(m_moduleAbbr + " CANcoder Angle (deg)", () -> getCANcoder().getDegrees());
-      m_swerveTab.addDouble(m_moduleAbbr + " FX Angle (deg)", () -> getAngle().getDegrees());
-      m_swerveTab.addDouble(m_moduleAbbr + " Velocity (m/s)", () -> getDriveVelocity());
-      m_swerveTab.addDouble(m_moduleAbbr + " Desired Velocity (m/s)", () -> m_desiredState.speedMetersPerSecond);
-      m_swerveTab.addDouble(m_moduleAbbr + " Desired Angle (deg)", () -> m_desiredState.angle.getDegrees());
-      m_swerveTab.addBoolean(m_moduleAbbr + " Jitter prevention enabled", () -> m_stateDeadband);
-      m_swerveTab.addDouble(m_moduleAbbr + " Drive Current (A)", () -> m_driveMotor.getSupplyCurrent());
-      m_swerveTab.addDouble(m_moduleAbbr + " Angle Current (A)", () -> m_angleMotor.getSupplyCurrent());
-    }
-  }
-
-  /**
-   * @return desired/ target state of module
-   */
-  public SwerveModuleState getDesiredState() {
-    return m_desiredState;
-  }
-  
   /**
    * @return desired drive velocity
    */
   public double getDesiredVelocity() {
     return getDesiredState().speedMetersPerSecond;
-  }
-  /**
-   * @return desired module steer angle
-   */
-  public Rotation2d getDesiredAngle() {
-    return getDesiredState().angle;
   }
   /**
    * @return drive velocity in meters per second
@@ -331,29 +292,55 @@ public class Module extends SubsystemBase {
   public double getDriveVelocityError() {
     return m_desiredState.speedMetersPerSecond - getDriveVelocity();
   }
-
   /**
-   * set module motor powers to 0
+   * @return get encoder position in meters
    */
-  public void stop() {
-    m_driveMotor.set(0);
-    m_angleMotor.set(0);
-  }
-
-  /**
-   * sets voltage of drive motor
-   * @param voltage voltage to set drive motor to in volts
-   */
-  public void setDriveVoltage(double voltage) {
-    m_driveMotor.set(ControlMode.PercentOutput, voltage / Constants.kRobotVoltage);
+  public double getDrivePosition(){
+    return Conversions.falconToMeters(
+          m_driveMotor.getSelectedSensorPosition(),
+          DriveConstants.kWheelCircumference,
+          DriveConstants.kDriveGearRatio);
   }
   
   /**
-   * sets voltage of steer motor
-   * @param voltage voltage to set steer motor to in volts
+   * @return module steer angle from Cancoder as Rotation2d with 0 being straight forward
    */
-  public void setSteerVoltage(double voltage) {
-    m_angleMotor.set(ControlMode.PercentOutput, voltage / Constants.kRobotVoltage);
+  public Rotation2d getCANcoder() {
+    return Rotation2d.fromDegrees(m_CANcoder.getAbsolutePosition() - m_angleOffset.getDegrees());
+  }
+  /**
+   * @return module steer angle from steer motor's integrated encoder as Rotation2d with 0 being straight forward
+   */
+  public Rotation2d getAngle() {
+    return Rotation2d.fromDegrees( Conversions.falconToDegrees(
+      m_angleMotor.getSelectedSensorPosition(), 
+      DriveConstants.kAngleGearRatio
+    ));
+  }
+  /**
+   * @return desired module steer angle
+   */
+  public Rotation2d getDesiredAngle() {
+    return getDesiredState().angle;
+  }
+  /**
+   * @return module steer rotational velocity in radians per second
+   */
+  public double getSteerVelocity() {
+    return Conversions.falconToRPM(m_angleMotor.getSelectedSensorVelocity(), DriveConstants.kAngleGearRatio) * 2 * Math.PI / 60;
+  }
+
+  private void setupShuffleboard() {
+    if (Constants.kUseTelemetry && RobotBase.isReal()) {
+      m_swerveTab.addDouble(m_moduleAbbr + " CANcoder Angle (deg)", () -> getCANcoder().getDegrees());
+      m_swerveTab.addDouble(m_moduleAbbr + " FX Angle (deg)", () -> getAngle().getDegrees());
+      m_swerveTab.addDouble(m_moduleAbbr + " Velocity (m/s)", () -> getDriveVelocity());
+      m_swerveTab.addDouble(m_moduleAbbr + " Desired Velocity (m/s)", () -> m_desiredState.speedMetersPerSecond);
+      m_swerveTab.addDouble(m_moduleAbbr + " Desired Angle (deg)", () -> m_desiredState.angle.getDegrees());
+      m_swerveTab.addBoolean(m_moduleAbbr + " Jitter prevention enabled", () -> m_stateDeadband);
+      m_swerveTab.addDouble(m_moduleAbbr + " Drive Current (A)", () -> m_driveMotor.getSupplyCurrent());
+      m_swerveTab.addDouble(m_moduleAbbr + " Angle Current (A)", () -> m_angleMotor.getSupplyCurrent());
+    }
   }
 
   //TODO: Possibly reimplement custom feedforward characterization
