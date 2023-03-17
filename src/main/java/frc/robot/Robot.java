9aba93614f976cd8ad05118d99aa29bf60b46462
AnthonyFurman <fura5196@lgsstudent.org>
2023-03-17 16:17:46 -0700

    fix robot non-operation for 7sec after auto

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -75,7 +75,6 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledInit() {
     CommandScheduler.getInstance().cancelAll();
-    m_robotContainer.autoChooserUpdate();
   }
 
   /** This function is called periodically when the robot is disabled */
