836777f5f6e6cad8b3a40b48e827baf2145d6226
Kyle-Eldridge <eldk6798@lgsstudent.org>
2023-03-26 15:06:29 -0700

    Change GoToNode back to non-SequentailCommandGroup (version that works)

--- src/main/java/frc/robot/commands/GoToNode.java
+++ src/main/java/frc/robot/commands/GoToNode.java
@@ -12,18 +12,16 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.auto.PathPlannerCommand;
 import frc.robot.controls.Operator;
 import frc.robot.subsystems.Drivetrain;
 
-public class GoToNode extends SequentialCommandGroup {
+public class GoToNode extends CommandBase {
 
   private Operator m_operator;
   private Drivetrain m_drive;
+  private CommandBase m_command;
   private DoubleSupplier m_yOffsetSup;
-  private SequentialCommandGroup m_command;
 
   /**
    * Uses PathPlanner to go to the selected node
@@ -35,17 +33,13 @@ public class GoToNode extends SequentialCommandGroup {
     m_drive = drive;
     m_yOffsetSup = yOffsetSup;
     addRequirements(drive);
-    m_command = new SequentialCommandGroup();
-    addCommands(
-      new InstantCommand(()->init()),
-      m_command
-    );
   }
 
   /**
    * Creates the PathPlanner command and schedules it
    */
-  public void init() {
+  @Override
+  public void initialize() {
     // Gets the current position of the robot for the start of the path
     PathPoint point1 = PathPoint.fromCurrentHolonomicState(
       m_drive.getPose(),
@@ -73,7 +67,7 @@ public class GoToNode extends SequentialCommandGroup {
     ).withControlLengths(0.001, 0.001);
 
     // Creates the command using the two points
-    CommandBase command = new PathPlannerCommand(
+    m_command = new PathPlannerCommand(
       new ArrayList<PathPoint>(List.of(point1, point2)),
       m_drive,
       false
@@ -81,34 +75,34 @@ public class GoToNode extends SequentialCommandGroup {
 
     double dist = m_drive.getPose().minus(scorePose).getTranslation().getNorm();
     if (dist > 3) {
-      command = new DoNothing();
+      m_command = new DoNothing();
       DriverStation.reportWarning("Alignment Path too long, doing nothing, GoToNode.java", false);
     }
     if (dist < 0.2) {
-      command = new DoNothing();
+      m_command = new DoNothing();
       DriverStation.reportWarning("Alignment Path too short, doing nothing, GoToNode.java", false);
     }
 
-    // Adds the command
-    m_command.addCommands(command);
+    // Starts the command
+    m_command.schedule();
   }
 
   /**
    * Stops the command and the drivetrain
    * @param interrupted If the command is interrupted
    */
-  // @Override
-  // public void end(boolean interrupted) {
-  //   m_command.cancel();
-  //   m_drive.stop();
-  // }
+  @Override
+  public void end(boolean interrupted) {
+    m_command.cancel();
+    m_drive.stop();
+  }
 
   /**
    * Returns if the PathPlannerCommand exists and is finished
    * @return If the GoToNode command is finished
    */
-  // @Override
-  // public boolean isFinished() {
-  //   return m_command != null && m_command.isFinished();
-  // }
+  @Override
+  public boolean isFinished() {
+    return m_command != null && m_command.isFinished();
+  }
 }
