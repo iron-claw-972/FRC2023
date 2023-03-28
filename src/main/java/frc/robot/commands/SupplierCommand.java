85655921a106a99ed83f29733e845931a5a5ce5c
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2023-03-27 20:44:25 -0700

    java docs

--- src/main/java/frc/robot/commands/SupplierCommand.java
+++ src/main/java/frc/robot/commands/SupplierCommand.java
@@ -5,12 +5,22 @@ import java.util.function.Supplier;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-
+/**
+ * Runs the given command when this command is initialized, and ends when it ends. 
+ * Useful for commands that are not created yet because the constructor parameters are not available until initialization.
+ */
 public class SupplierCommand extends CommandBase {
     
     Supplier<Command> m_commandSupplier;
     Command m_command;
 
+    /**
+     * Runs the given command when this command is initialized, and ends when it ends. 
+     * Useful for commands that are not created yet because the constructor parameters are not available until initialization.
+     * 
+     * @param commandSupplier A Supplier to the command to run
+     * @param Subsystem all subsystems that may be required to run the command supplied
+     */
     public SupplierCommand(Supplier<Command> commandSupplier, Subsystem... Subsystem){
         addRequirements(Subsystem);
         m_commandSupplier = commandSupplier;
