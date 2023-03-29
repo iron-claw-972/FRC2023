ebb2b7b242551d72302774c07d0f80eb0b18ff6c
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2023-03-29 16:53:40 -0700

    Add mean clculator

--- src/main/java/frc/robot/util/StatisticsUtil.java
+++ src/main/java/frc/robot/util/StatisticsUtil.java
@@ -3,24 +3,38 @@ package frc.robot.util;
 import java.util.ArrayList;
 
 public class StatisticsUtil {
-  public static double stdDev(ArrayList<Double> data){
-    double[] dataArray = new double[data.size()];
-    for(int i = 0; i < data.size(); i++){
-        dataArray[i] = data.get(i);
+  public static double[] arrayListToArray(ArrayList<Double> arrayList){
+    double[] array = new double[arrayList.size()];
+    for(int i = 0; i < arrayList.size(); i++){
+        array[i] = arrayList.get(i);
     }
-    return stdDev(dataArray);
+    return array;
+  }
+
+  public static double mean(ArrayList<Double> data){
+    return mean(arrayListToArray(data));
+  }
+
+  public static double mean(double[] data){
+    double mean = 0;
+    for (int i = 0; i < data.length; i++) {
+      mean += data[i];
+    }
+    mean /= data.length;
+    return mean;
+  }
+
+  public static double stdDev(ArrayList<Double> data){
+    return stdDev(arrayListToArray(data));
   }
 
   public static double stdDev(double[] data){
     if(data.length == 0){
       return 0;
     }
-          
-    double mean = 0;
-    for (int i = 0; i < data.length; i++) {
-      mean += data[i];
-    }
-    mean /= data.length;
+    
+    double mean = mean(data);
+    
     double total = 0;
     for (int i = 0; i < data.length; i++) {
       total += Math.pow(data[i] - mean, 2);
