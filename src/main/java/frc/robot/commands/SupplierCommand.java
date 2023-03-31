70ea1a7bf03588d3223e9d46110a6656940f3548
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2023-03-31 15:22:01 -0700

    Intentaiton fix

--- src/main/java/frc/robot/commands/SupplierCommand.java
+++ src/main/java/frc/robot/commands/SupplierCommand.java
@@ -10,41 +10,41 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
  * Useful for commands that are not created yet because the constructor parameters are not available until initialization.
  */
 public class SupplierCommand extends CommandBase {
-    
-    Supplier<Command> m_commandSupplier;
-    Command m_command;
-
-    /**
-     * Runs the given command when this command is initialized, and ends when it ends. 
-     * Useful for commands that are not created yet because the constructor parameters are not available until initialization.
-     * 
-     * @param commandSupplier A Supplier to the command to run
-     * @param Subsystem all subsystems that may be required to run the command supplied
-     */
-    public SupplierCommand(Supplier<Command> commandSupplier, Subsystem... Subsystem){
-        addRequirements(Subsystem);
-        m_commandSupplier = commandSupplier;
-    } 
-
-    @Override
-    public final void initialize() {
-        m_command = m_commandSupplier.get();
-        m_command.initialize();
-    }
-
-    @Override
-    public final void execute() {
-        m_command.execute();
-    }
-
-    @Override
-    public final void end(boolean interrupted) {
-        m_command.end(interrupted);
-    }
-
-    @Override
-    public final boolean isFinished() {
-        return m_command.isFinished();
-    }
-
+  
+  Supplier<Command> m_commandSupplier;
+  Command m_command;
+  
+  /**
+   * Runs the given command when this command is initialized, and ends when it ends. 
+   * Useful for commands that are not created yet because the constructor parameters are not available until initialization.
+   * 
+   * @param commandSupplier A Supplier to the command to run
+   * @param Subsystem all subsystems that may be required to run the command supplied
+   */
+  public SupplierCommand(Supplier<Command> commandSupplier, Subsystem... Subsystem){
+    addRequirements(Subsystem);
+    m_commandSupplier = commandSupplier;
+  } 
+  
+  @Override
+  public final void initialize() {
+    m_command = m_commandSupplier.get();
+    m_command.initialize();
+  }
+  
+  @Override
+  public final void execute() {
+    m_command.execute();
+  }
+  
+  @Override
+  public final void end(boolean interrupted) {
+    m_command.end(interrupted);
+  }
+  
+  @Override
+  public final boolean isFinished() {
+    return m_command.isFinished();
+  }
+  
 }
