1197ed07bac3a8c614eb6696f516f750b261ebcd
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2023-04-06 12:26:07 -0700

    change camera position

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -65,7 +65,7 @@ public class VisionConstants {
           "Left_Camera",
           new Transform3d(
             new Translation3d(Units.inchesToMeters(8.996), Units.inchesToMeters(6.48), Units.inchesToMeters(37.44)),
-            new Rotation3d(Math.PI, Units.degreesToRadians(18), 0)
+            new Rotation3d(0, Units.degreesToRadians(18), 0)
           ))
         ,
         new Pair<String, Transform3d>(
