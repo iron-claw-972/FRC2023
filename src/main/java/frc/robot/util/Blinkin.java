b92b94b4d3042d7a587ff377a58a8cc9d99b0c5b
chem7397 <113394068+chem7397@users.noreply.github.com>
2023-04-10 14:51:52 -0700

    Added Enum Colors

--- src/main/java/frc/robot/util/Blinkin.java
+++ src/main/java/frc/robot/util/Blinkin.java
@@ -11,5 +11,36 @@ public class Blinkin {
   public Blinkin() {
     m_ledController = new CANSparkMax(0, MotorType.kBrushless);
   }
+
+
+  enum Colors{
+    RainbowRainbowPalette(-0.99),
+    HotPink(0.57),
+    DarkRed(0.59),
+    Red(0.61),
+    Orange(0.65),
+    Gold(0.67),
+    Yellow(0.69),
+    Lime(0.73),
+    Green(0.77),
+    Aqua(0.81),
+    SkyBlue(0.83),
+    DarkBlue(0.85),
+    blue(0.87),
+    BlueViolet(0.89),
+    Violet(0.91),
+    White(0.93),
+    Gray(0.95),
+    DarkGray(0.97),
+    Black(0.99);
+
+    public final double m_id;
+    Colors(double id){
+      m_id = id;
+    }
+
+    
+
+  }
   
 }
